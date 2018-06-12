3d80cb3e1a5448cfaafc89cd689bcc1aa241686c
TerryFei <v-tiafe@microsoft.com>
2018-06-12 12:47:50 +0800

    Push project Get selected item by code

---
+++ OneCodeTeam/Get-selected-item-by-code-22610/[C#]-Get selected item by code/C#/CSASPNETGetSelectedValueOfAutoCompleteExtender/App_Code/AutoComplete.cs
@@ -0,0 +1,89 @@
+﻿/****************************** Module Header ******************************\
+* Module Name:    AutoComplete.cs
+* Project:        CSASPNETGetSelectedValueOfAutoCompleteExtender
+* Copyright (c) Microsoft Corporation
+*
+* This WebService is used to search for movies under the conditions
+* 
+* This source is subject to the Microsoft Public License.
+* See http://www.microsoft.com/en-us/openness/licenses.aspx#MPL.
+* All other rights reserved.
+* 
+* THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, 
+* EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED 
+* WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+\*****************************************************************************/
+
+using System;
+using System.Collections;
+using System.Configuration;
+using System.Data;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Web.Services.Protocols;
+
+[WebService(Namespace = "http://tempuri.org/")]
+// To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+[System.Web.Script.Services.ScriptService]
+public class AutoComplete : System.Web.Services.WebService
+{
+    private List<string> _movies = new List<string>();
+
+    public AutoComplete()
+    {
+        _movies.Add("a1");
+        _movies.Add("a2");
+        _movies.Add("b1");
+        _movies.Add("China");
+        _movies.Add("c21");
+        _movies.Add("Dead");
+        _movies.Add("Dear");
+        _movies.Add("Dream");
+        _movies.Add("Empty");
+        _movies.Add("Egg");
+        _movies.Add("Flower");
+        _movies.Add("Floor");
+        _movies.Add("Great");
+        _movies.Add("g");
+        _movies.Add("h1");
+        _movies.Add("h2");
+        _movies.Add("i1");
+        _movies.Add("jeep");
+        _movies.Add("k");
+        _movies.Add("Loop");
+        _movies.Add("Man");
+        _movies.Add("Nice");
+        _movies.Add("O1");
+        _movies.Add("Pear");
+        _movies.Add("Queen");
+        _movies.Add("Rest");
+        _movies.Add("S1");
+        _movies.Add("S2");
+        _movies.Add("T1 ");
+        _movies.Add("T2 ");
+        _movies.Add("US");
+        _movies.Add("UK");
+        _movies.Add("V");
+        _movies.Add("W");
+        _movies.Add("X");
+        _movies.Add("Y");
+        _movies.Add("Z");
+    }
+
+    [WebMethod]
+    public string[] GetMovies(string prefixText, int count)
+    {
+        if (count == 0)
+        {
+            count = 10;
+        }
+
+        var matches = (from m in _movies
+                       where m.StartsWith(prefixText, StringComparison.CurrentCultureIgnoreCase)
+                       select m).Take(count);
+        return matches.ToArray();
+    }
+}
+
---
+++ OneCodeTeam/Get-selected-item-by-code-22610/[C#]-Get selected item by code/C#/CSASPNETGetSelectedValueOfAutoCompleteExtender/Default.aspx.cs
@@ -0,0 +1,100 @@
+﻿/****************************** Module Header ******************************\
+* Module Name:    Default.aspx.cs
+* Project:        CSASPNETGetSelectedValueOfAutoCompleteExtender
+* Copyright (c) Microsoft Corporation
+*
+* This demo shows how you can get selected item when you select an item from 
+* list populated by AutoCompleteExtender at code behind.
+* 
+* This source is subject to the Microsoft Public License.
+* See http://www.microsoft.com/en-us/openness/licenses.aspx#MPL.
+* All other rights reserved.
+* 
+* THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, 
+* EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED 
+* WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+\*****************************************************************************/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace CSASPNETGetSelectedValueOfAutoCompleteExtender
+{
+    public partial class _Default : System.Web.UI.Page
+    {
+        // Sql connection.
+        static string connetionString = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+        SqlConnection connection = new SqlConnection(connetionString);
+
+        protected void Page_Load(object sender, EventArgs e)
+        {           
+            BindGrid();
+        }
+
+        /// <summary>
+        /// Bind datatable to GridView
+        /// </summary>
+        private void BindGrid()
+        {
+            // Query string
+            string queryString = "SELECT [Id], [Keywords], [Count] FROM [KeywordsStatistics]";
+            SqlDataAdapter adapter = new SqlDataAdapter();
+
+            // Set query string
+            adapter.SelectCommand = new SqlCommand(queryString, connection);
+
+            // Open connection
+            connection.Open();
+
+            // Sql data is stored DataSet.                 
+            DataSet sqlData = new DataSet();
+            adapter.Fill(sqlData, "KeywordsStatistics");
+
+            // Close connection
+            connection.Close();
+
+            // Bind datatable to GridView
+            gdvKeyword.DataSource = sqlData.Tables[0];
+            gdvKeyword.DataBind();
+        }
+
+        // database operation
+        protected void LinkButton1_Click(object sender, EventArgs e)
+        {
+            // Get the selected value of the AutoCompleteExtender
+            string hifvalue = hf1.Value;
+
+            string queryString = "SELECT id FROM [KeywordsStatistics] where Keywords=@Keyword";
+            SqlParameter para = new SqlParameter("Keyword", hifvalue);
+            SqlCommand command = new SqlCommand(queryString, connection);
+            command.Parameters.Add(para);
+            connection.Open();
+            SqlDataReader reader = command.ExecuteReader();
+
+            // If there is a corresponding search record, update the statistics. Otherwise, add a new record.
+            if (reader.HasRows)
+            {
+                // Update the statistics.
+                queryString = "update KeywordsStatistics set Count=count+1 where Keywords=@Keyword";
+            }
+            else
+            {
+                // Add a new record.
+                queryString = "Insert into [KeywordsStatistics](Keywords, Count)values(@Keyword,1)";
+            }
+            reader.Close();
+            command.CommandText = queryString;
+            command.ExecuteNonQuery();
+            connection.Close();
+
+            BindGrid();
+        }
+    }
+}
