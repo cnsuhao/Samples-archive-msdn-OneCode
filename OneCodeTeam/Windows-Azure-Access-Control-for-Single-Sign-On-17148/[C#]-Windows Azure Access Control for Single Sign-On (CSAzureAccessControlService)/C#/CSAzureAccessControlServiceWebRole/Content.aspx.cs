10221688b24f4c690c48971af246839d22637739
TerryFei <v-tiafe@microsoft.com>
2018-06-12 11:46:03 +0800

    Push project Windows Azure Access Control for Single Sign-On (CSAzureAccessControlService)

---
+++ OneCodeTeam/Windows-Azure-Access-Control-for-Single-Sign-On-17148/[C#]-Windows Azure Access Control for Single Sign-On (CSAzureAccessControlService)/C#/CSAzureAccessControlServiceWebRole/Content.aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Threading;
+
+namespace CSAzureAccessControlServiceWebRole
+{
+    public partial class Content : System.Web.UI.Page
+    {
+        public string userName = string.Empty;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            userName = Thread.CurrentPrincipal.Identity.Name;
+        }
+    }
+}
