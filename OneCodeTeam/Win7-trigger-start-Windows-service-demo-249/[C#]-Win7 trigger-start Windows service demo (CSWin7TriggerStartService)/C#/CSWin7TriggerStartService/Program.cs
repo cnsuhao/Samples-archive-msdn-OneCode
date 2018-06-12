90e15efaab00b8c0710a0b389aa97792bf1c234a
TerryFei <v-tiafe@microsoft.com>
2018-06-12 09:46:52 +0800

    Push project Win7 trigger-start Windows service demo (CSWin7TriggerStartService)

---
+++ OneCodeTeam/Win7-trigger-start-Windows-service-demo-249/[C#]-Win7 trigger-start Windows service demo (CSWin7TriggerStartService)/C#/CSWin7TriggerStartService/Program.cs
@@ -0,0 +1,39 @@
+﻿/****************************** Module Header ******************************\
+* Module Name:  Program.cs
+* Project:      CSWin7TriggerStartService
+* Copyright (c) Microsoft Corporation.
+* 
+* The file defines the entry point of the application.
+* 
+* This source is subject to the Microsoft Public License.
+* See http://www.microsoft.com/en-us/openness/licenses.aspx#MPL.
+* All other rights reserved.
+* 
+* THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, 
+* EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED 
+* WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+\***************************************************************************/
+
+#region Using directives
+using System.ServiceProcess;
+#endregion
+
+
+namespace CSWin7TriggerStartService
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main()
+        {
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[] 
+			{ 
+				new Service() 
+			};
+            ServiceBase.Run(ServicesToRun);
+        }
+    }
+}
