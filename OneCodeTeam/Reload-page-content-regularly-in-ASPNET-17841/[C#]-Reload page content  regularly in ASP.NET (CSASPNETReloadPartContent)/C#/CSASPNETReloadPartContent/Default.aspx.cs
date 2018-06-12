8bf5b7b4140608b8669157aafb068b3974211c80
TerryFei <v-tiafe@microsoft.com>
2018-06-12 11:57:03 +0800

    Push project Reload page content  regularly in ASP.NET (CSASPNETReloadPartContent)

---
+++ OneCodeTeam/Reload-page-content-regularly-in-ASPNET-17841/[C#]-Reload page content  regularly in ASP.NET (CSASPNETReloadPartContent)/C#/CSASPNETReloadPartContent/Default.aspx.cs
@@ -0,0 +1,33 @@
+﻿/****************************** Module Header ******************************\
+* Module Name:    Default.aspx.cs
+* Project:        CSASPNETReloadPartContent
+* Copyright (c) Microsoft Corporation
+*
+* The project illustrates how to reload part of page content regularly.
+* 
+* This source is subject to the Microsoft Public License.
+* See http://www.microsoft.com/en-us/openness/licenses.aspx#MPL.
+* All other rights reserved.
+* 
+* THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, 
+* EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED 
+* WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+\*****************************************************************************/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CSASPNETReloadPartContent
+{
+    public partial class _Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
