1b15f8e5e8a5357b6fbdd69a156283cc4c045009
TerryFei <v-tiafe@microsoft.com>
2018-06-12 13:12:52 +0800

    Push project Windows Phone SkyDrive Note

---
+++ OneCodeTeam/Windows-Phone-SkyDrive-Note-23698/[VB]-Windows Phone SkyDrive Note/VB/VBWP8SkydriveNote/My Project/AssemblyInfo.vb
@@ -0,0 +1,39 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+Imports System.Resources
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+<Assembly: AssemblyTitle("VBWP8SkydriveNote")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyConfiguration("")> 
+<Assembly: AssemblyCompany("Microsoft Corp.")> 
+<Assembly: AssemblyProduct("VBWP8SkydriveNote")> 
+<Assembly: AssemblyCopyright("Copyright © Microsoft Corp. 2010")> 
+<Assembly: AssemblyTrademark("")> 
+<Assembly: AssemblyCulture("")> 
+
+' Setting ComVisible to false makes the types in this assembly not visible 
+' to COM components.  If you need to access a type in this assembly from 
+' COM, set the ComVisible attribute to true on that type.
+<assembly: ComVisible(false)>
+
+' The following GUID is for the ID of the typelib if this project is exposed to COM
+<assembly: Guid("fcd861dd-00e0-4eef-8819-0a1264db56fc")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Revision and Build Numbers 
+' by using the '*' as shown below:
+<assembly: AssemblyVersion("1.0.0.0")>
+<assembly: AssemblyFileVersion("1.0.0.0")>
+<assembly: NeutralResourcesLanguageAttribute("en-US")>
