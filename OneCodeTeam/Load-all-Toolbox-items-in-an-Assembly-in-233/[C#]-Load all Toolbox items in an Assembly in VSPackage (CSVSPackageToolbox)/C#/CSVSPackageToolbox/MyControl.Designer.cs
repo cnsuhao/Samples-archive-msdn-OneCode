75f3c0db9d38c581df0333a80d447d9edce37f5f
TerryFei <v-tiafe@microsoft.com>
2018-06-12 09:44:47 +0800

    Push project Load all Toolbox items in an Assembly in VSPackage (CSVSPackageToolbox)

---
+++ OneCodeTeam/Load-all-Toolbox-items-in-an-Assembly-in-233/[C#]-Load all Toolbox items in an Assembly in VSPackage (CSVSPackageToolbox)/C#/CSVSPackageToolbox/MyControl.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace MyCompany.CSVSToolbox
+{
+    partial class MyControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.tbFileName = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnBrowse.Location = new System.Drawing.Point(293, 15);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(63, 20);
+            this.btnBrowse.TabIndex = 0;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // tbFileName
+            // 
+            this.tbFileName.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbFileName.Location = new System.Drawing.Point(3, 16);
+            this.tbFileName.Name = "tbFileName";
+            this.tbFileName.Size = new System.Drawing.Size(284, 20);
+            this.tbFileName.TabIndex = 1;
+            // 
+            // MyControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.tbFileName);
+            this.Controls.Add(this.btnBrowse);
+            this.Name = "MyControl";
+            this.Size = new System.Drawing.Size(359, 47);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.TextBox tbFileName;
+    }
+}
---
+++ OneCodeTeam/Load-all-Toolbox-items-in-an-Assembly-in-233/[C#]-Load all Toolbox items in an Assembly in VSPackage (CSVSPackageToolbox)/C#/CSVSPackageToolbox/MyControl.cs
@@ -0,0 +1,45 @@
+﻿/****************************** Module Header ******************************\
+ Module Name:  MyControl.cs
+ Project:      CSVSPackageToolbox
+ Copyright (c) Microsoft Corporation.
+ 
+ This UserControl will be displayed as the content of the ToolWindow.
+ 
+ This source is subject to the Microsoft Public License.
+ See http://www.microsoft.com/en-us/openness/resources/licenses.aspx#MPL.
+ All other rights reserved.
+ 
+ THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, 
+ EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED 
+ WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+\***************************************************************************/
+
+using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace MyCompany.CSVSToolbox
+{
+    // Set the display name and custom bitmap to use for this item.
+    // The build action for the bitmap must be "Embedded Resource".
+    [DisplayName("ToolboxMemberDemo")]
+    [Description("Custom toolbox item from package LoadToolboxMembers.")]
+    [ToolboxItem(true)]
+    [ToolboxBitmap(typeof(MyControl), "MyControl.bmp")]
+    public partial class MyControl : UserControl
+    {
+        public MyControl()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog opg = new OpenFileDialog();
+            opg.Multiselect = false;
+            if (opg.ShowDialog() == DialogResult.OK)
+                this.tbFileName.Text = opg.FileName;
+        }
+    }
+}
