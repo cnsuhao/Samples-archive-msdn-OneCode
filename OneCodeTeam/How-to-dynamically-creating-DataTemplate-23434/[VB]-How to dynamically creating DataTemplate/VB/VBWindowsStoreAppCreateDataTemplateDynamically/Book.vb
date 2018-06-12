a952a1187ea2fabba0d40e3fb3d00356014eb15d
TerryFei <v-tiafe@microsoft.com>
2018-06-12 13:03:24 +0800

    Push project How to dynamically creating DataTemplate

---
+++ OneCodeTeam/How-to-dynamically-creating-DataTemplate-23434/[VB]-How to dynamically creating DataTemplate/VB/VBWindowsStoreAppCreateDataTemplateDynamically/Book.vb
@@ -0,0 +1,115 @@
+﻿'***************************** Module Header ******************************\
+' Module Name:    Book.vb
+' Project:        VBWindowsStoreAppCreateDataTemplateDynamically
+' Copyright (c) Microsoft Corporation.
+' 
+' This class is used to initialize data in Book collection
+' 
+' This source is subject to the Microsoft Public License.
+' See http://www.microsoft.com/en-us/openness/licenses.aspx#MPL.
+' All other rights reserved.
+' 
+' THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, 
+' EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED 
+' WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+'**************************************************************************/
+
+
+Imports System.Collections.Generic
+
+Public Class Book
+    Public Property Name() As String
+        Get
+            Return m_Name
+        End Get
+        Set(value As String)
+            m_Name = value
+        End Set
+    End Property
+    Private m_Name As String
+    Public Property Price() As Decimal
+        Get
+            Return m_Price
+        End Get
+        Set(value As Decimal)
+            m_Price = value
+        End Set
+    End Property
+    Private m_Price As Decimal
+    Public Property Author() As String
+        Get
+            Return m_Author
+        End Get
+        Set(value As String)
+            m_Author = value
+        End Set
+    End Property
+    Private m_Author As String
+
+    Public Shared Function GetBooks() As List(Of Book)
+        Dim Books As New List(Of Book)()
+
+        Books.Add(New Book() With { _
+            .Author = "Allen", _
+            .Price = 29.99D, _
+            .Name = "War" _
+        })
+        Books.Add(New Book() With { _
+            .Author = "Carter", _
+            .Price = 35D, _
+            .Name = "Fighting Like A Man" _
+        })
+        Books.Add(New Book() With { _
+            .Author = "Rose", _
+            .Price = 39.99D, _
+            .Name = "Tomorrow" _
+        })
+        Books.Add(New Book() With { _
+            .Author = "Daisy", _
+            .Price = 99D, _
+            .Name = "Last Three Days" _
+        })
+        Books.Add(New Book() With { _
+            .Author = "Mary", _
+            .Price = 10D, _
+            .Name = "Fire Wall" _
+        })
+        Books.Add(New Book() With { _
+            .Author = "Ray", _
+            .Price = 19.99D, _
+            .Name = "Lie" _
+        })
+        Books.Add(New Book() With { _
+            .Author = "Sherry", _
+            .Price = 45.5D, _
+            .Name = "Three Wolves" _
+        })
+        Books.Add(New Book() With { _
+            .Author = "Lisa", _
+            .Price = 36.99D, _
+            .Name = "Beauty" _
+        })
+        Books.Add(New Book() With { _
+            .Author = "Judy", _
+            .Price = 12D, _
+            .Name = "The One" _
+        })
+        Books.Add(New Book() With { _
+            .Author = "Jack", _
+            .Price = 88.99D, _
+            .Name = "Chosen by God" _
+        })
+        Books.Add(New Book() With { _
+            .Author = "May", _
+            .Price = 130D, _
+            .Name = "The Magic" _
+        })
+        Books.Add(New Book() With { _
+            .Author = "Vivian", _
+            .Price = 299.99D, _
+            .Name = "Who is the Murder" _
+        })
+
+        Return Books
+    End Function
+End Class
---
+++ OneCodeTeam/How-to-dynamically-creating-DataTemplate-23434/[VB]-How to dynamically creating DataTemplate/VB/VBWindowsStoreAppCreateDataTemplateDynamically/MainPage.xaml.vb
@@ -0,0 +1,76 @@
+﻿'***************************** Module Header ******************************\
+' Module Name:    MainPage.xaml.vb
+' Project:        VBWindowsStoreAppCreateDataTemplateDynamically
+' Copyright (c) Microsoft Corporation.
+' 
+' This sample demonstrates how to create DataTemplate Dynamically
+' 
+' This source is subject to the Microsoft Public License.
+' See http://www.microsoft.com/en-us/openness/licenses.aspx#MPL.
+' All other rights reserved.
+' 
+' THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, 
+' EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED 
+' WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+'**************************************************************************/
+
+Imports Windows.UI.Xaml.Navigation
+Imports System.Text
+Imports Windows.UI.Xaml.Markup
+
+' The Basic Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234237
+
+''' <summary>
+''' A basic page that provides characteristics common to most applications.
+''' </summary>
+Public NotInheritable Class MainPage
+    Inherits Common.LayoutAwarePage
+
+#Region "Common methods"
+
+    ''' <summary>
+    ''' The the event handler for the click event of the link in the footer. 
+    ''' </summary>
+    ''' <param name="sender">
+    ''' The sender.
+    ''' </param>
+    ''' <param name="e">
+    ''' The event arguments.
+    ''' </param>
+    Private Async Sub FooterClick(sender As Object, e As RoutedEventArgs)
+        Dim hyperlinkButton As HyperlinkButton = TryCast(sender, HyperlinkButton)
+        If hyperlinkButton IsNot Nothing Then
+            Await Windows.System.Launcher.LaunchUriAsync(New Uri(hyperlinkButton.Tag.ToString()))
+        End If
+    End Sub
+
+    Public Sub NotifyUser(message As String)
+        Me.StatusText.Text = message
+    End Sub
+
+#End Region
+
+    Protected Overrides Sub OnNavigatedTo(e As NavigationEventArgs)
+        Dim Books As List(Of Book) = Book.GetBooks()
+        BookGridView.ItemsSource = Books
+        BookListView.ItemsSource = Books
+
+        Dim sb As New StringBuilder()
+        sb.Append("<DataTemplate xmlns=""http://schemas.microsoft.com/winfx/2006/xaml/presentation"">")
+        sb.Append("<Grid Width=""200"" Height=""100"">")
+        sb.Append("<StackPanel>")
+        sb.Append("<StackPanel Orientation=""Horizontal"" Margin=""3,3,0,3""><TextBlock Text=""Name:"" Style=""{StaticResource AppBodyTextStyle}"" Margin=""0,0,5,0""/><TextBlock Text=""{Binding Name}"" Style=""{StaticResource AppBodyTextStyle}""/></StackPanel>")
+        sb.Append("<StackPanel Orientation=""Horizontal"" Margin=""3,3,0,3""><TextBlock Text=""Price:"" Style=""{StaticResource AppBodyTextStyle}"" Margin=""0,0,5,0""/><TextBlock Text=""{Binding Price}"" Style=""{StaticResource AppBodyTextStyle}""/></StackPanel>")
+        sb.Append("<StackPanel Orientation=""Horizontal"" Margin=""3,3,0,3""><TextBlock Text=""Author:"" Style=""{StaticResource AppBodyTextStyle}"" Margin=""0,0,5,0""/><TextBlock Text=""{Binding Author}"" Style=""{StaticResource AppBodyTextStyle}""/></StackPanel>")
+        sb.Append("</StackPanel>")
+        sb.Append("</Grid>")
+        sb.Append("</DataTemplate>")
+
+        Dim datatemplate As DataTemplate = DirectCast(XamlReader.Load(sb.ToString()), DataTemplate)
+        BookGridView.ItemTemplate = datatemplate
+        BookListView.ItemTemplate = datatemplate
+
+        MyBase.OnNavigatedTo(e)
+    End Sub
+
+End Class
