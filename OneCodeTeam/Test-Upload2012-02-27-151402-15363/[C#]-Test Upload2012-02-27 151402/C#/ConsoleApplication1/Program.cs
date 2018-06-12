6f1be92d5fb614d6bdfba83d503bb483cd86243a
TerryFei <v-tiafe@microsoft.com>
2018-06-12 11:08:35 +0800

    Push project Test Upload2012-02-27 15:14:02

---
+++ OneCodeTeam/Test-Upload2012-02-27-151402-15363/[C#]-Test Upload2012-02-27 151402/C#/ConsoleApplication1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            dynamic expandoObject = new ExpandoObject();
+            expandoObject.Hello = (Action)(() => Console.WriteLine("Hello, How are you?"));
+            expandoObject.Name = "Matt";
+
+            Console.WriteLine(expandoObject.Name);
+            expandoObject.Hello();
+
+            Console.WriteLine();
+            Console.WriteLine();
+            foreach(var prop in expandoObject.GetType().GetProperties())
+            {
+                Console.WriteLine(prop.Name);
+            }
+        }
+    }
+
+}
