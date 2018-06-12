7f35cacca5e5105627688b847eb934c0176b3d0c
TerryFei <v-tiafe@microsoft.com>
2018-06-12 11:26:02 +0800

    Push project Navigation usage in WPF (VBWPFNavigationUsage)

---
+++ OneCodeTeam/Navigation-usage-in-WPF-VBWPFNavigationUsage-16021/[VB]-Navigation usage in WPF (VBWPFNavigationUsage)/VB/VBWPFNavigationUsage/FramePage.xaml.vb
@@ -0,0 +1,29 @@
+﻿'**************************** Module Header ******************************\
+' Module Name:  FramePage.xaml.vb
+' Project:      VBWPFNavigationUsage
+' Copyright (c) Microsoft Corporation.
+'
+' The sample demonstrates navigation usages in a WPF application.
+'
+' This source is subject to the Microsoft Public License.
+' See http://www.microsoft.com/en-us/openness/resources/licenses.aspx#MPL
+' All other rights reserved.
+'
+' THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, 
+' EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED 
+' WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+'**************************************************************************/
+
+Imports System.Text
+
+Namespace VBWPFNavigationUsage
+    ''' <summary>
+    ''' Interaction logic for FrameUsage.xaml
+    ''' </summary>
+    Partial Public Class FrameUsage
+        Inherits Page
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+    End Class
+End Namespace
---
+++ OneCodeTeam/Navigation-usage-in-WPF-VBWPFNavigationUsage-16021/[VB]-Navigation usage in WPF (VBWPFNavigationUsage)/VB/VBWPFNavigationUsage/MainPage.xaml.vb
@@ -0,0 +1,60 @@
+﻿'**************************** Module Header ******************************\
+' Module Name:  MainPage.xaml.vb
+' Project:      VBWPFNavigationUsage
+' Copyright (c) Microsoft Corporation.
+'
+' The sample demonstrates navigation usages in a WPF application.
+'
+' This source is subject to the Microsoft Public License.
+' See http://www.microsoft.com/en-us/openness/resources/licenses.aspx#MPL
+' All other rights reserved.
+'
+' THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, 
+' EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED 
+' WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+'**************************************************************************/
+
+Imports System.Text
+
+Namespace VBWPFNavigationUsage
+    Partial Public Class MainPage
+        Inherits Page
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+
+        Private Sub OnHyperlink(ByVal sender As Object, ByVal e As RoutedEventArgs)
+            Me.NavigationService.Navigate(New Uri("Page1.xaml", UriKind.Relative))
+        End Sub
+
+        Private Sub OnNavagateToObject(ByVal sender As Object, ByVal e As RoutedEventArgs)
+            Dim obj As New MyDummy() With {.Property1 = "Hello", .Property2 = "everyone"}
+            Me.NavigationService.Navigate(obj)
+        End Sub
+
+        Private Sub OnNavagateToPage(ByVal sender As Object, ByVal e As RoutedEventArgs)
+            Me.NavigationService.Navigate(New Uri("FramePage.xaml", UriKind.Relative))
+        End Sub
+    End Class
+
+    Public Class MyDummy
+        Private _Property1 As String
+        Private _Property2 As String
+        Public Property Property1() As String
+            Get
+                Return _Property1
+            End Get
+            Set(ByVal value As String)
+                _Property1 = value
+            End Set
+        End Property
+        Public Property Property2() As String
+            Get
+                Return _Property2
+            End Get
+            Set(ByVal value As String)
+                _Property2 = value
+            End Set
+        End Property
+    End Class
+End Namespace
