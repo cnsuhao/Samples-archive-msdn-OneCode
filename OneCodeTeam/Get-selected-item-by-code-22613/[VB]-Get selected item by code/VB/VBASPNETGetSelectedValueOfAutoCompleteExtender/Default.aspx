5cf8f9cb92e718bf580ac945c6168a5d687e2bdb
TerryFei <v-tiafe@microsoft.com>
2018-06-12 12:48:25 +0800

    Push project Get selected item by code

---
+++ OneCodeTeam/Get-selected-item-by-code-22613/[VB]-Get selected item by code/VB/VBASPNETGetSelectedValueOfAutoCompleteExtender/Default.aspx
@@ -0,0 +1,52 @@
+﻿<%@ Page Language="vb" AutoEventWireup="false" CodeBehind="Default.aspx.vb" Inherits="VBASPNETGetSelectedValueOfAutoCompleteExtender.VBASPNETGetSelectedValueOfAutoCompleteExtender._Default" %>
+
+<%@ Register Assembly="AjaxControlToolkit" Namespace="AjaxControlToolkit" TagPrefix="AjaxControlToolkit" %>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head id="Head1" runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <AjaxControlToolkit:ToolkitScriptManager CombineScripts="false" runat="server" EnablePartialRendering="true">
+    </AjaxControlToolkit:ToolkitScriptManager>        
+    <div>
+        <table border="1">
+            <caption>
+                Search Movies
+            </caption>
+            <tr>
+                <td colspan="4">
+                    You can input part or complete of the movie name, and then select one item.
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    MovieName:
+                    <asp:TextBox ID="tbMovies" runat="server" Text=''></asp:TextBox>
+                    <AjaxControlToolkit:AutoCompleteExtender ID="AutoCompleteExtender1" BehaviorID="ACE"
+                        runat="server" TargetControlID="tbMovies" ServicePath="AutoComplete.asmx" ServiceMethod="GetMovies"
+                        MinimumPrefixLength="1" CompletionInterval="10" CompletionSetCount="10" EnableCaching="true" />
+                </td>
+                <td>
+                    <asp:GridView runat="server" ID="gdvKeyword" EnableModelValidation="True" AutoGenerateColumns="False"
+                        DataKeyNames="Id">
+                        <Columns>
+                            <asp:BoundField DataField="Id" HeaderText="Id" InsertVisible="False" ReadOnly="True"
+                                SortExpression="Id" />
+                            <asp:BoundField DataField="Keywords" HeaderText="Keywords" SortExpression="Keywords" />
+                            <asp:BoundField DataField="Count" HeaderText="Count" SortExpression="Count" />
+                        </Columns>
+                    </asp:GridView>
+                </td>
+            </tr>
+        </table>
+        <asp:LinkButton ID="LinkButton1" runat="server" OnClick="LinkButton1_Click"></asp:LinkButton>
+        <asp:HiddenField ID="hf1" runat="server" />
+    </div>
+
+    <script src="AutoComplete.js" type="text/javascript"></script>
+
+    </form>
+</body>
+</html>
---
+++ OneCodeTeam/Get-selected-item-by-code-22613/[VB]-Get selected item by code/VB/VBASPNETGetSelectedValueOfAutoCompleteExtender/Default.aspx.vb
@@ -0,0 +1,85 @@
+﻿'****************************** Module Header ******************************\
+' Module Name:    Default.aspx.vb
+' Project:        VBASPNETGetSelectedValueOfAutoCompleteExtender
+' Copyright (c) Microsoft Corporation
+'
+' This demo shows how you can get selected item when you select an item from 
+' list populated by AutoCompleteExtender at code behind.
+'
+' This source is subject to the Microsoft Public License.
+' See http://www.microsoft.com/en-us/openness/licenses.aspx#MPL.
+' All other rights reserved.
+'
+' THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, 
+' EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED 
+' WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+'*****************************************************************************/
+Imports System.Data.SqlClient
+
+Namespace VBASPNETGetSelectedValueOfAutoCompleteExtender
+    Partial Public Class _Default
+        Inherits System.Web.UI.Page
+        ' Sql connection.
+        Shared connetionString As String = ConfigurationManager.ConnectionStrings("ConnectionString").ConnectionString
+        Private connection As New SqlConnection(connetionString)
+
+        Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs) Handles Me.Load
+            BindGrid()
+        End Sub
+
+        ''' <summary>
+        ''' Bind datatable to GridView
+        ''' </summary>
+        Private Sub BindGrid()
+            ' Query string
+            Dim queryString As String = "SELECT [Id], [Keywords], [Count] FROM [KeywordsStatistics]"
+            Dim adapter As New SqlDataAdapter()
+
+            ' set query string
+            adapter.SelectCommand = New SqlCommand(queryString, connection)
+
+            ' Open connection
+            connection.Open()
+
+            ' Sql data is stored DataSet.                 
+            Dim sqlData As New DataSet()
+            adapter.Fill(sqlData, "KeywordsStatistics")
+
+            ' Close connection
+            connection.Close()
+
+            ' Bind datatable to GridView
+            gdvKeyword.DataSource = sqlData.Tables(0)
+            gdvKeyword.DataBind()
+        End Sub
+
+        ' database operation
+        Protected Sub LinkButton1_Click(ByVal sender As Object, ByVal e As EventArgs)
+            ' Get the selected value of the AutoCompleteExtender
+            Dim hifvalue As String = hf1.Value
+
+            Dim queryString As String = "SELECT id FROM [KeywordsStatistics] where Keywords=@Keyword"
+            Dim para As New SqlParameter("Keyword", hifvalue)
+            Dim command As New SqlCommand(queryString, connection)
+            command.Parameters.Add(para)
+            connection.Open()
+            Dim reader As SqlDataReader = command.ExecuteReader()
+
+            ' If there is a corresponding search record, update the statistics. Otherwise, add a new record.
+            If reader.HasRows Then
+                ' Update the statistics.
+                queryString = "update KeywordsStatistics set Count=count+1 where Keywords=@Keyword"
+            Else
+                ' Add a new record.
+                queryString = "Insert into [KeywordsStatistics](Keywords, Count)values(@Keyword,1)"
+            End If
+            reader.Close()
+            command.CommandText = queryString
+            command.ExecuteNonQuery()
+            connection.Close()
+
+            BindGrid()
+        End Sub
+
+    End Class
+End Namespace
