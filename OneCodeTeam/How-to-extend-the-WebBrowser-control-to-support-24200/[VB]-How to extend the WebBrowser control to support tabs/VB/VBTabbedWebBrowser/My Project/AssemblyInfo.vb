2d7e48b82c76d6a9e57cc023617485a88a49d80f
TerryFei <v-tiafe@microsoft.com>
2018-06-12 13:14:55 +0800

    Push project How to extend the WebBrowser control to support tabs

---
+++ OneCodeTeam/How-to-extend-the-WebBrowser-control-to-support-24200/[VB]-How to extend the WebBrowser control to support tabs/VB/VBTabbedWebBrowser/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("VBTabbedWebBrowser")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("Microsoft Corporation")> 
+<Assembly: AssemblyProduct("VBTabbedWebBrowser")> 
+<Assembly: AssemblyCopyright("Copyright © Microsoft Corporation 2013")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("2876a293-0ea5-4bde-8786-a2d9c3746088")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
