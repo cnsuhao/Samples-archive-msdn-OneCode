bd2dcae7ba5361a1d759ff025b34b6c585e9b3ef
TerryFei <v-tiafe@microsoft.com>
2018-06-12 13:34:47 +0800

    Push project Caculate how many words in txt file using regular expression

---
+++ OneCodeTeam/Caculate-how-many-words-in-txt-file-using-regular-28118/Readme.md
@@ -0,0 +1,58 @@
+# Caculate how many words in txt file using regular expression
+## Requires
+* Visual Studio 2010
+## License
+* Apache License, Version 2.0
+## Technologies
+* .NET Development
+* Windows Desktop App Development
+## Topics
+* Regular Expression
+## IsPublished
+* True
+## ModifiedDate
+* 2014-04-10 02:07:04
+## Description
+
+<hr>
+<div><a href="http://blogs.msdn.com/b/onecode" style="margin-top:3px"><img alt="" src="http://bit.ly/onecodesampletopbanner">
+</a></div>
+<h2>Introduction</h2>
+<p><span style="font-family:&quot;Calibri Light&quot;,&quot;sans-serif&quot;">The sample demonstrates the following features:
+</span></p>
+<p><span style="font-family:&quot;Calibri Light&quot;,&quot;sans-serif&quot;">1. How to calculate how many words in txt file using regular expression.
+</span></p>
+<p><span style="font-family:&quot;Calibri Light&quot;,&quot;sans-serif&quot;">2.&nbsp;How to calculate the occurrence count of&nbsp;a word.
+</span></p>
+<h2>Running the Sample</h2>
+<p class="MsoListParagraphCxSpFirst" style="text-indent:-.25in"><span style=""><span style="">1.<span style="font:7.0pt &quot;Times New Roman&quot;">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+</span></span></span>Press Ctrl &#43; F5 to run this sample.</p>
+<p class="MsoListParagraphCxSpLast" style="text-indent:-.25in"><span style=""><span style="">2.<span style="font:7.0pt &quot;Times New Roman&quot;">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+</span></span></span>Type a valid file path.</p>
+<p class="MsoNormal"><span style=""><img src="/site/view/file/112638/1/image.png" alt="" width="575" height="290" align="middle">
+</span></p>
+<p class="MsoListParagraphCxSpFirst" style="text-indent:-.25in"><span style=""><span style="">3.<span style="font:7.0pt &quot;Times New Roman&quot;">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+</span></span></span>Choose the appropriate option from the menu.</p>
+<p class="MsoListParagraphCxSpLast"><span style=""><img src="/site/view/file/112639/1/image.png" alt="" width="576" height="293" align="middle">
+</span></p>
+<h2></h2>
+<h2></h2>
+<h2>Using the Code</h2>
+<p class="MsoNormal">The following function returns the total number of words in the input text file.</p>
+<p class="MsoNormal" style=""></p>
+<p class="MsoNormal" style=""></p>
+<p class="MsoNormal" style=""></p>
+<p class="MsoNormal" style="">The following function returns the number of occurrences of a specific word in the input text file.</p>
+<p class="MsoNormal" style=""></p>
+<p class="MsoNormal" style="">MSDN: .NET Framework Regular Expressions<br>
+<a href="http://msdn.microsoft.com/en-us/library/hs600312.aspx">http://msdn.microsoft.com/en-us/library/hs600312.aspx</a></p>
+<p class="MsoNormal" style="">MSDN: Regular Expression Language: Quick Reference<br>
+<a href="http://msdn.microsoft.com/en-us/library/az24scfc.aspx">http://msdn.microsoft.com/en-us/library/az24scfc.aspx</a>
+</p>
+<p class="MsoNormal" style=""></p>
+<p style="line-height:0.6pt; color:white">Microsoft All-In-One Code Framework is a free, centralized code sample library driven by developers' real-world pains and needs. The goal is to provide customer-driven code samples for all Microsoft development technologies,
+ and reduce developers' efforts in solving typical programming tasks. Our team listens to developers’ pains in the MSDN forums, social media and various DEV communities. We write code samples based on developers’ frequently asked programming tasks, and allow
+ developers to download them with a short sample publishing cycle. Additionally, we offer a free code sample request service. It is a proactive way for our developer community to obtain code samples directly from Microsoft.</p>
+<hr>
+<div><a href="http://go.microsoft.com/?linkid=9759640" style="margin-top:3px"><img alt="" src="http://bit.ly/onecodelogo">
+</a></div>
---
+++ OneCodeTeam/Caculate-how-many-words-in-txt-file-using-regular-28118/[VB]-Caculate how many words in txt file using regular expression/VB/VBTextWordCount/Module1.vb
@@ -0,0 +1,110 @@
+﻿'****************************** Module Header ******************************\
+' Module Name:    Module1.vb
+' Project:        VBTextWordCount
+' Copyright (c) Microsoft Corporation
+'
+' The sample demonstrates the following features:
+'
+' 1. How to calculate how many words in txt file using regular expression.
+'
+' 2. How to calculate the occurrence count of a word.
+' 
+' This source is subject to the Microsoft Public License.
+' See http://www.microsoft.com/en-us/openness/resources/licenses.aspx#MPL.
+' All other rights reserved.
+'
+' THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND,
+' EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED
+' WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+'*****************************************************************************/
+
+Imports System.Text.RegularExpressions
+Imports System.IO
+
+Module Module1
+
+    Sub Main()
+        Dim choice As Integer = 0
+
+        ' Input the text file Location
+        Console.Write("Input File Location : ")
+
+        Dim filename As String = Console.ReadLine()
+
+        While choice <> 3
+            printMenu()
+            Try
+                choice = Convert.ToInt32(Console.ReadLine())
+                Select Case choice
+                    Case 1
+                        Console.WriteLine("Total word count = " & totalWords(filename))
+                        Exit Select
+                    Case 2
+                        Console.WriteLine("Enter the specific word : ")
+                        Dim word As String = Console.ReadLine()
+                        Console.WriteLine("Total word occurrence = " & wordCount(filename, word))
+                        Exit Select
+                    Case 3
+                        Exit Select
+                End Select
+            Catch e As System.FormatException
+                Console.WriteLine("Please enter valid choice!")
+            End Try
+        End While
+    End Sub
+
+    ' <summary>
+    ' This function prints the main menu.
+    ' </summary>
+
+    Private Sub printMenu()
+        Console.WriteLine(vbLf & "1. Calculate Total Number of words in the Input file")
+        Console.WriteLine("2. Calculate occurence count of a word")
+        Console.WriteLine("3. Exit")
+    End Sub
+
+    ' <summary>
+    ' This function counts the total number of words in the input text file.
+    ' </summary>
+    ' <param name="filename">File Name</param>
+    ' <returns>Return the total word count</returns>
+
+    Private Function totalWords(filename As String) As Integer
+        Dim count As Integer = 0
+        Dim sr As New StreamReader(filename)
+        Dim input As String
+        Dim pattern As String = "\b\w+\b"
+
+        While sr.Peek() >= 0
+            input = sr.ReadLine()
+            Dim rgx As New Regex(pattern, RegexOptions.IgnoreCase)
+            Dim matches As MatchCollection = rgx.Matches(input)
+            count += matches.Count
+        End While
+        sr.Close()
+        Return count
+    End Function
+
+    ' <summary>
+    ' This function counts the number of times a specific word is present in the input text file.
+    ' </summary>
+    ' <param name="filename">File Name</param>
+    ' <returns>Return the word count for a specific word</returns>
+
+    Private Function wordCount(filename As String, word As String) As Integer
+        Dim count As Integer = 0
+        Dim sr As New StreamReader(filename)
+        Dim input As String
+
+        Dim pattern As String = word
+        While sr.Peek() >= 0
+            input = sr.ReadLine()
+            Dim rgx As New Regex(pattern, RegexOptions.IgnoreCase)
+            Dim matches As MatchCollection = rgx.Matches(input)
+            count += matches.Count
+        End While
+        sr.Close()
+        Return count
+    End Function
+
+End Module
