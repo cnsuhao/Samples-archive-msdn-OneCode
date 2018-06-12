07174bf1d65135763e0729c4324242d7b75b9f83
TerryFei <v-tiafe@microsoft.com>
2018-06-12 13:15:24 +0800

    Push project Create a custom asynchronous code activity

---
+++ OneCodeTeam/Create-a-custom-asynchronous-code-activity-24203/[C#]-Create a custom asynchronous code activity/C#/CSWF4AsyncHttpGetActivity/AsyncHttpGet.cs
@@ -0,0 +1,49 @@
+﻿/****************************** Module Header ******************************\
+* Module Name:  AsyncHttpGet.cs
+* Project:		CSWF4AsyncHttpGetActivity
+* Copyright (c) Microsoft Corporation.
+* 
+* This source is subject to the Microsoft Public License.
+* See http://www.microsoft.com/en-us/openness/licenses.aspx#MPL
+* All other rights reserved.
+* 
+* THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, 
+* EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED 
+* WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+\***************************************************************************/
+using System.Activities;
+using System;
+using System.Net;
+using System.IO;
+
+namespace CSWF4AsyncHttpGetActivity
+{
+    public class AsyncHttpGetActivity : AsyncCodeActivity<string>
+    {
+        public InArgument<string> Uri { get; set; }
+        protected override IAsyncResult BeginExecute(AsyncCodeActivityContext context,
+                                                     AsyncCallback callback,
+                                                     object state)
+        {
+            WebRequest request = HttpWebRequest.Create(this.Uri.Get(context));
+            context.UserState = request;
+            return request.BeginGetResponse(callback, state);
+        }
+
+        protected override string EndExecute(AsyncCodeActivityContext context,
+                                             IAsyncResult result)
+        {
+
+            WebRequest request = context.UserState as WebRequest;
+            using (WebResponse response = request.EndGetResponse(result))
+            {
+                using (StreamReader reader =
+                    new StreamReader(response.GetResponseStream()))
+                {
+                    return reader.ReadToEnd();
+                }
+            }
+
+        }
+    }
+}
---
+++ OneCodeTeam/Create-a-custom-asynchronous-code-activity-24203/[C#]-Create a custom asynchronous code activity/C#/CSWF4AsyncHttpGetActivity/Program.cs
@@ -0,0 +1,31 @@
+﻿/****************************** Module Header ******************************\
+* Module Name:  Program.cs
+* Project:		CSWF4AsyncHttpGetActivity
+* Copyright (c) Microsoft Corporation.
+* 
+* This source is subject to the Microsoft Public License.
+* See http://www.microsoft.com/en-us/openness/licenses.aspx#MPL
+* All other rights reserved.
+* 
+* THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, 
+* EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED 
+* WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+\***************************************************************************/
+
+using System;
+using System.Linq;
+using System.Activities;
+using System.Activities.Statements;
+
+namespace CSWF4AsyncHttpGetActivity
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Activity workflow1 = new Workflow1();
+            WorkflowInvoker.Invoke(workflow1);
+        }
+    }
+}
