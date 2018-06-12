bea34ba84e1fea4646ecf2fa68d3fb9a52f79337
TerryFei <v-tiafe@microsoft.com>
2018-06-12 09:43:24 +0800

    Push project Basic VSPackage created in C# (CSVSPackage)

---
+++ OneCodeTeam/Basic-VSPackage-created-in-C-CSVSPackage-224/[C#]-Basic VSPackage created in C# (CSVSPackage)/C#/CSVSPackage/Guids.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Company.CSVSPackage
+{
+    static class GuidList
+    {
+        public const string guidCSVSPackagePkgString = "1cb46a03-e9b9-4acc-a5b4-530234c5916d";
+        public const string guidCSVSPackageCmdSetString = "40b77c85-ba3e-4097-87b7-69bebe873276";
+
+        public static readonly Guid guidCSVSPackageCmdSet = new Guid(guidCSVSPackageCmdSetString);
+    };
+}
