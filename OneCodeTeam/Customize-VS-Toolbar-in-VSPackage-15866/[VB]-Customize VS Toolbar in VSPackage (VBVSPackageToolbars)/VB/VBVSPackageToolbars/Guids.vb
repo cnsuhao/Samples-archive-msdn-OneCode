ef7cdd51a841790baab68c356fa1ca5ba9a77eee
TerryFei <v-tiafe@microsoft.com>
2018-06-12 11:18:11 +0800

    Push project Customize VS Toolbar in VSPackage (VBVSPackageToolbars)

---
+++ OneCodeTeam/Customize-VS-Toolbar-in-VSPackage-15866/[VB]-Customize VS Toolbar in VSPackage (VBVSPackageToolbars)/VB/VBVSPackageToolbars/Guids.vb
@@ -0,0 +1,11 @@
+﻿Imports System
+
+Class GuidList
+    Private Sub New()
+    End Sub
+
+    Public Const guidVBVSPackageToolbarsPkgString As String = "63dd5d66-d98c-478f-94f1-0587cb7fa58c"
+    Public Const guidVBVSPackageToolbarsCmdSetString As String = "d5e5d042-4c2b-497c-afef-32d493b8fa3f"
+
+    Public Shared ReadOnly guidVBVSPackageToolbarsCmdSet As New Guid(guidVBVSPackageToolbarsCmdSetString)
+End Class
---
+++ OneCodeTeam/Customize-VS-Toolbar-in-VSPackage-15866/[VB]-Customize VS Toolbar in VSPackage (VBVSPackageToolbars)/VB/VBVSPackageToolbars/PkgCmdID.vb
@@ -0,0 +1,14 @@
+﻿' PkgCmdID.vb
+Imports System
+
+Class PkgCmdIDList
+    Private Sub New()
+    End Sub
+
+    Public Const cmdidMyCommand As UInteger = &H100
+
+
+    Public Const cmdidMCItem1 As UInteger = &H130
+    Public Const cmdidMCItem2 As UInteger = &H131
+    Public Const cmdidMCItem3 As UInteger = &H132
+End Class
