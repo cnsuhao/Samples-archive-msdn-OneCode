245ee7d568e64d5fcbb9bfbc10d94bc6eef6c81e
TerryFei <v-tiafe@microsoft.com>
2018-06-12 11:25:20 +0800

    Push project ASP.NET master page demo (VBASPNETMasterPage)

---
+++ OneCodeTeam/ASPNET-page-demo-VBASPNETMasterPage-15945/[VB]-ASP.NET master page demo (VBASPNETMasterPage)/VB/VBASPNETMasterPage/ContentPage.aspx.vb
@@ -0,0 +1,33 @@
+﻿'****************************** Module Header ******************************\
+'Module Name:  ContentPage.aspx.vb
+'Project:      VBASPNETMasterPage
+'Copyright (c) Microsoft Corporation.
+'
+'This page is used to show content page
+'
+'This source is subject to the Microsoft Public License.
+'See http://www.microsoft.com/en-us/openness/resources/licenses.aspx#MPL
+'All other rights reserved.
+'
+'THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, 
+'EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED 
+'WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+'***************************************************************************/
+
+
+
+Public Class ContentPage
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+
+    End Sub
+
+    Protected Sub Button1_Click(ByVal sender As Object, ByVal e As EventArgs) Handles Button1.Click
+        Dim lbMasterPageHello As Label = TryCast(Master.FindControl("lbHello"), Label)
+
+        If lbMasterPageHello IsNot Nothing Then
+            lbMasterPageHello.Text = "Hello, " & txtName.Text & "!"
+        End If
+    End Sub
+End Class
---
+++ OneCodeTeam/ASPNET-page-demo-VBASPNETMasterPage-15945/[VB]-ASP.NET master page demo (VBASPNETMasterPage)/VB/VBASPNETMasterPage/NestedContentPageA.aspx
@@ -0,0 +1,6 @@
+﻿<%@ Page Title="" Language="vb" AutoEventWireup="false" MasterPageFile="~/NestedMasterPage.master" CodeBehind="NestedContentPageA.aspx.vb" Inherits="VBASPNETMasterPage.NestedContentPageA" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="NestedContentHolder" runat="server">
+    <div style="background-color: #F9F9F9; width: 300px;">
+        <h3>Nested Content PageA</h3>
+    </div>
+</asp:Content>
