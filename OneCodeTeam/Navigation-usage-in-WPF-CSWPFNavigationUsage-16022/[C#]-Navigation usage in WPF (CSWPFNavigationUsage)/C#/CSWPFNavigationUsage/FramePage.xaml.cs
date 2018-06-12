3d8293c1e607bc787899f6bd92c559be03570b50
TerryFei <v-tiafe@microsoft.com>
2018-06-12 11:26:11 +0800

    Push project Navigation usage in WPF (CSWPFNavigationUsage)

---
+++ OneCodeTeam/Navigation-usage-in-WPF-CSWPFNavigationUsage-16022/[C#]-Navigation usage in WPF (CSWPFNavigationUsage)/C#/CSWPFNavigationUsage/FramePage.xaml.cs
@@ -0,0 +1,31 @@
+﻿/****************************** Module Header ******************************\
+ Module Name:  FramePage.xaml.cs
+ Project:      CSWPFNavigationUsage
+ Copyright (c) Microsoft Corporation.
+
+ The sample demonstrates navigation usages in a WPF application.
+
+ This source is subject to the Microsoft Public License.
+ See http://www.microsoft.com/en-us/openness/resources/licenses.aspx#MPL
+ All other rights reserved.
+
+ THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, 
+ EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED 
+ WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+\***************************************************************************/
+
+using System.Windows.Controls;
+
+namespace CSWPFNavigationUsage
+{
+    /// <summary>
+    /// Interaction logic for FrameUsage.xaml
+    /// </summary>
+    public partial class FrameUsage : Page
+    {
+        public FrameUsage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ OneCodeTeam/Navigation-usage-in-WPF-CSWPFNavigationUsage-16022/[C#]-Navigation usage in WPF (CSWPFNavigationUsage)/C#/CSWPFNavigationUsage/Page1.xaml.cs
@@ -0,0 +1,46 @@
+﻿/****************************** Module Header ******************************\
+ Module Name:  Page1.xaml.cs
+ Project:      CSWPFNavigationUsage
+ Copyright (c) Microsoft Corporation.
+
+ The sample demonstrates navigation usages in a WPF application.
+
+ This source is subject to the Microsoft Public License.
+ See http://www.microsoft.com/en-us/openness/resources/licenses.aspx#MPL
+ All other rights reserved.
+
+ THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, 
+ EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED 
+ WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+\***************************************************************************/
+
+using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+
+
+namespace CSWPFNavigationUsage
+{
+    /// <summary>
+    /// Interaction logic for Page1.xaml
+    /// </summary>
+    public partial class Page1 : Page
+    {
+        public Page1()
+        {
+            InitializeComponent();
+            // Get a reference to the NavigationService that navigated to this Page
+            NavigationService ns = NavigationService.GetNavigationService(this);
+        }
+        void hyperlink_Click(object sender, RoutedEventArgs e)
+        {
+            // Create a pack URI
+            Uri uri = new Uri("AnotherPage.xaml", UriKind.Relative);
+            // Get the navigation service that was used to 
+            // navigate to this page, and navigate to 
+            // AnotherPage.xaml
+            this.NavigationService.Navigate(uri);
+        }
+    }
+}
