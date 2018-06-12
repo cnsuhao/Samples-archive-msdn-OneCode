53dd1bd6a87f5479721f146981debc35abbb3a18
TerryFei <v-tiafe@microsoft.com>
2018-06-12 14:04:35 +0800

    Push project How to write Service Principal Name on a Computer/User object (VS2012)

---
+++ OneCodeTeam/How-to-write-Service-Principal-Name-on-a-112135/[VB]-How to write Service Principal Name on a ComputerUser object (VS2012)/VB/VBDsWriteAccountSPN2012/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("VBDsWriteAccountSPN2012")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("")> 
+<Assembly: AssemblyProduct("VBDsWriteAccountSPN2012")> 
+<Assembly: AssemblyCopyright("Copyright ©  2015")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("623fbebe-2cba-4962-adbc-16c0f629c4f6")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
