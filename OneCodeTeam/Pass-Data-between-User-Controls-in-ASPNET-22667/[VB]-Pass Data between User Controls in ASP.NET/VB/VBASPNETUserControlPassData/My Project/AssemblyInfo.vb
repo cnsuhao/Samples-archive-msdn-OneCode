95c2b221f64543868fbe120b31e37c7738257f1c
TerryFei <v-tiafe@microsoft.com>
2018-06-12 12:57:39 +0800

    Push project Pass Data between User Controls in ASP.NET

---
+++ OneCodeTeam/Pass-Data-between-User-Controls-in-ASPNET-22667/[VB]-Pass Data between User Controls in ASP.NET/VB/VBASPNETUserControlPassData/My Project/AssemblyInfo.vb
@@ -0,0 +1,34 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+<Assembly: AssemblyTitle("VBASPNETUserControlPassData")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("Microsoft Corp.")> 
+<Assembly: AssemblyProduct("VBASPNETUserControlPassData")> 
+<Assembly: AssemblyCopyright("Copyright © Microsoft Corp. 2013")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("103d5b15-7f01-4116-8db4-c1c8f853aff2")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
---
+++ OneCodeTeam/Pass-Data-between-User-Controls-in-ASPNET-22667/[VB]-Pass Data between User Controls in ASP.NET/VB/VBASPNETUserControlPassData/UserControl/UserControl1.ascx
@@ -0,0 +1,13 @@
+﻿<%@ Control Language="vb" AutoEventWireup="false" CodeBehind="UserControl1.ascx.vb" Inherits="VBASPNETUserControlPassData.UserControl1" %>
+<style type="text/css">
+    .UserControl1
+    {
+        border:1px solid;
+        border-color:Red;
+    }
+</style>
+<div class="UserControl1">
+   User Control 1 element:
+   <br />
+   User Control 1 Message: <asp:Label ID="lbPublicVariable" runat="server"></asp:Label>
+</div>
