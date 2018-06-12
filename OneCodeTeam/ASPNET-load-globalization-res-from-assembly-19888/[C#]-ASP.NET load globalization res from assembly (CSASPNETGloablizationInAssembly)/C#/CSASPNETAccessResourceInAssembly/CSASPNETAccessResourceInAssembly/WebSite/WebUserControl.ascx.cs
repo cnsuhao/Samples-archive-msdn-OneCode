60ed5220add60f16a74e1ba41eafc05e0d0ec2b8
TerryFei <v-tiafe@microsoft.com>
2018-06-12 12:14:55 +0800

    Push project ASP.NET load globalization res from assembly (CSASPNETGloablizationInAssembly)

---
+++ OneCodeTeam/ASPNET-load-globalization-res-from-assembly-19888/[C#]-ASP.NET load globalization res from assembly (CSASPNETGloablizationInAssembly)/C#/CSASPNETAccessResourceInAssembly/CSASPNETAccessResourceInAssembly/WebSite/WebUserControl.ascx.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSASPNETAccessResourceInAssembly.WebSite
+{
+    public partial class WebUserControl : System.Web.UI.UserControl
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ OneCodeTeam/ASPNET-load-globalization-res-from-assembly-19888/[C#]-ASP.NET load globalization res from assembly (CSASPNETGloablizationInAssembly)/C#/CSASPNETAccessResourceInAssembly/CSASPNETAssembly/WebUserControl.ascx.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSASPNETAssembly
+{
+    public partial class WebUserControl : System.Web.UI.UserControl
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
