acfcd0899d31ece61806436bacb6fa2f9d214932
TerryFei <v-tiafe@microsoft.com>
2018-06-12 09:30:56 +0800

    Push project ASP.NET cascading drop-down-list (CSASPNETCascadingDropDownList)

---
+++ OneCodeTeam/ASPNET-cascading-drop-down-list-141/[C#]-ASP.NET cascading drop-down-list (CSASPNETCascadingDropDownList)/C#/CSASPNETCascadingDropDownList/Default.aspx.cs
@@ -0,0 +1,47 @@
+﻿/****************************** Module Header ******************************\
+Module Name:  Default.aspx.cs
+Project:      CSASPNETCascadingDropDown
+Copyright (c) Microsoft Corporation.
+ 
+This page is set to be the start page.
+ 
+This source is subject to the Microsoft Public License.
+See http://www.microsoft.com/en-us/openness/resources/licenses.aspx#MPL
+All other rights reserved.
+ 
+THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, 
+EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED 
+WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+\***************************************************************************/
+
+#region Using directives
+using System;
+using System.Collections;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+#endregion
+
+
+namespace CSASPNETCascadingDropDownList
+{
+    public partial class Default : System.Web.UI.Page
+    {
+
+        /// <summary>
+        /// Page Load event
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
