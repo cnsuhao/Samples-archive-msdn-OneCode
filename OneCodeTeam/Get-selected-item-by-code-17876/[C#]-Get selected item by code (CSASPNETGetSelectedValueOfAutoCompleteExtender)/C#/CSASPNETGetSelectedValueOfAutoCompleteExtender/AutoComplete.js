648aca26c8e540943611a1fc0d8af36bfa9c01f5
TerryFei <v-tiafe@microsoft.com>
2018-06-12 12:02:57 +0800

    Push project Get selected item by code (CSASPNETGetSelectedValueOfAutoCompleteExtender)

---
+++ OneCodeTeam/Get-selected-item-by-code-17876/[C#]-Get selected item by code (CSASPNETGetSelectedValueOfAutoCompleteExtender)/C#/CSASPNETGetSelectedValueOfAutoCompleteExtender/AutoComplete.js
@@ -0,0 +1,12 @@
+﻿function pageLoad() {
+    //Register for add_itemSelected event
+    $find('ACE').add_itemSelected(OnItemSelected);
+}
+
+function OnItemSelected(sender, eventArgs) {
+    //Assign value to HiddenField
+    var hf = $get("hf1");
+    hf.value = (eventArgs.get_value());
+    //__doPostBack,Operation selected value in the service side code
+    __doPostBack('LinkButton1', '');
+}
