21ae4f4d3b73e99c48cb44eb3880cf6454434a92
TerryFei <v-tiafe@microsoft.com>
2018-06-12 13:13:30 +0800

    Push project Windows Phone Custom Style Button

---
+++ OneCodeTeam/Windows-Phone-Custom-Style-Button-23702/[VB]-Windows Phone Custom Style Button/VB/VBWP8CustomStyleButton/MainPage.xaml.vb
@@ -0,0 +1,52 @@
+﻿'****************************** Module Header ******************************\
+' Module Name:    MainPage.xaml.vb
+' Project:        VBWP8CustomStyleButton
+' Copyright (c) Microsoft Corporation
+'
+' This sample demonstrates how to create your own style button. 
+' 
+' This source is subject to the Microsoft Public License.
+' See http://www.microsoft.com/en-us/openness/licenses.aspx#MPL.
+' All other rights reserved.
+' 
+' THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, 
+' EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED 
+' WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+'*****************************************************************************/
+
+Imports System
+Imports System.Threading
+Imports System.Windows.Controls
+Imports Microsoft.Phone.Controls
+Imports Microsoft.Phone.Shell
+
+Partial Public Class MainPage
+    Inherits PhoneApplicationPage
+
+    ' Constructor
+    Public Sub New()
+        InitializeComponent()
+
+        SupportedOrientations = SupportedPageOrientation.Portrait Or SupportedPageOrientation.Landscape
+
+        ' Sample code to localize the ApplicationBar
+        'BuildLocalizedApplicationBar()
+
+    End Sub
+
+    ' Sample code for building a localized ApplicationBar
+    'Private Sub BuildLocalizedApplicationBar()
+    '    ' Set the page's ApplicationBar to a new instance of ApplicationBar.
+    '    ApplicationBar = New ApplicationBar()
+
+    '    ' Create a new button and set the text value to the localized string from AppResources.
+    '    Dim appBarButton As New ApplicationBarIconButton(New Uri("/Assets/AppBar/appbar.add.rest.png", UriKind.Relative))
+    '    appBarButton.Text = AppResources.AppBarButtonText
+    '    ApplicationBar.Buttons.Add(appBarButton)
+
+    '    ' Create a new menu item with the localized string from AppResources.
+    '    Dim appBarMenuItem As New ApplicationBarMenuItem(AppResources.AppBarMenuItemText)
+    '    ApplicationBar.MenuItems.Add(appBarMenuItem)
+    'End Sub
+
+End Class
