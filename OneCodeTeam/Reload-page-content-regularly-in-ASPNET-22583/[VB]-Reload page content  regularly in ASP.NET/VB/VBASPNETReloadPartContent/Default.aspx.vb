72782281c55a02629de39ca6a933e6de74a9aded
TerryFei <v-tiafe@microsoft.com>
2018-06-12 12:43:54 +0800

    Push project Reload page content  regularly in ASP.NET

---
+++ OneCodeTeam/Reload-page-content-regularly-in-ASPNET-22583/[VB]-Reload page content  regularly in ASP.NET/VB/VBASPNETReloadPartContent/Default.aspx.vb
@@ -0,0 +1,23 @@
+﻿'****************************** Module Header ******************************\
+' Module Name:  Default.aspx.cs
+' Project:      VBASPNETReloadPartContent
+' Copyright (c) Microsoft Corporation
+'
+' The project illustrates how to reload part of page content regularly.
+' 
+' This source is subject to the Microsoft Public License.
+' See http://www.microsoft.com/en-us/openness/licenses.aspx#MPL.
+' All other rights reserved.
+' 
+' THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, 
+' EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED 
+' WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+'***************************************************************************/
+Partial Public Class _Default
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+
+    End Sub
+
+End Class
---
+++ OneCodeTeam/Reload-page-content-regularly-in-ASPNET-22583/[VB]-Reload page content  regularly in ASP.NET/VB/VBASPNETReloadPartContent/Refresh.aspx.vb
@@ -0,0 +1,53 @@
+﻿'****************************** Module Header ******************************\
+' Module Name:  Refresh.aspx.cs
+' Project:      VBASPNETReloadPartContent
+' Copyright (c) Microsoft Corporation
+'
+' The project illustrates how to reload part of page content regularly.
+' This page is used to load the data. 
+' 
+' This source is subject to the Microsoft Public License.
+' See http://www.microsoft.com/en-us/openness/licenses.aspx#MPL.
+' All other rights reserved.
+' 
+' THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, 
+' EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED 
+' WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+'***************************************************************************/
+Partial Public Class Refresh
+    Inherits System.Web.UI.Page
+
+    'Declare a table to store data:custom or query from Database
+    Dim dtResult As DataTable = Nothing
+
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        'Put your logic here.
+        BindResult()
+    End Sub
+
+    'Here i create a table to store the time of refresh,you can put your logic here instead of it.
+    Private Sub BindResult()
+        'In the actual scene, you may not need operating the session, here is just to test
+        If Session("dtResult") IsNot Nothing Then
+            dtResult = DirectCast(Session("dtResult"), DataTable)
+        Else
+            dtResult = New DataTable()
+            dtResult.Columns.Add("Id")
+            dtResult.Columns.Add("Time")
+        End If
+
+        Dim dr As DataRow = dtResult.NewRow()
+        dr("Id") = dtResult.Rows.Count + 1
+        dr("Time") = DateTime.Now.ToString()
+        dtResult.Rows.Add(dr)
+
+        'Save data to Session, in a 
+        Session("dtResult") = dtResult
+        'Bind data to GridView
+
+        rptResult.DataSource = dtResult
+        rptResult.DataBind()
+    End Sub
+
+End Class
