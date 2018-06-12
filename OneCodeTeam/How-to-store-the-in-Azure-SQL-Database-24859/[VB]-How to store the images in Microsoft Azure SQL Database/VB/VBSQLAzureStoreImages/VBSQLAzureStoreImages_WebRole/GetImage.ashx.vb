ffe1812e1bd493af991ecb89e7195c76f091e085
TerryFei <v-tiafe@microsoft.com>
2018-06-12 13:16:48 +0800

    Push project How to store the images in Microsoft Azure SQL Database

---
+++ OneCodeTeam/How-to-store-the-in-Azure-SQL-Database-24859/[VB]-How to store the images in Microsoft Azure SQL Database/VB/VBSQLAzureStoreImages/VBSQLAzureStoreImages_WebRole/GetImage.ashx.vb
@@ -0,0 +1,47 @@
+﻿'**************************** Module Header ******************************\
+' Module Name:  GetImage.ashx.vb
+' Project:      VBSQLAzureStoreImages_WebRole
+' Copyright (c) Microsoft Corporation.
+' 
+' This sample demonstrates how to store images in Windows Azure SQL Server.
+' We can use this class to get the images from SQL Azure and return to the client.
+' 
+' This source is subject to the Microsoft Public License.
+' See http://www.microsoft.com/en-us/openness/licenses.aspx#MPL.
+' All other rights reserved.
+' 
+' THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, 
+' EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED 
+' WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+'**************************************************************************/
+
+Imports System.Linq
+
+''' <summary>
+''' Use it to get the images from SQL Azure.
+''' </summary>
+Public Class GetImage
+    Implements IHttpHandler
+    Private imagesDb As New ImagesContext()
+
+    Public Sub ProcessRequest(ByVal context As HttpContext) Implements IHttpHandler.ProcessRequest
+        context.Response.ContentType = "image/jpeg"
+
+        Dim imageId As Int32 = Int32.Parse(context.Request.QueryString("ImageId"))
+        Dim image As ImagesTable = (
+            From i In imagesDb.ImagesTable
+            Where i.ImageId = imageId
+            Select i).FirstOrDefault()
+        If image IsNot Nothing Then
+            context.Response.BinaryWrite(image.ImageData)
+        Else
+            context.Response.Write("No this Image")
+        End If
+    End Sub
+
+    Public ReadOnly Property IsReusable() As Boolean Implements IHttpHandler.IsReusable
+        Get
+            Return False
+        End Get
+    End Property
+End Class
---
+++ OneCodeTeam/How-to-store-the-in-Azure-SQL-Database-24859/[VB]-How to store the images in Microsoft Azure SQL Database/VB/VBSQLAzureStoreImages/VBSQLAzureStoreImages_WebRole/Model.vb
@@ -0,0 +1,61 @@
+﻿'**************************** Module Header ******************************\
+' Module Name:  Model.vb
+' Project:      VBSQLAzureStoreImages_WebRole
+' Copyright (c) Microsoft Corporation.
+' 
+' This sample demonstrates how to store images in Windows Azure SQL Server.
+' This file includes the model of Entity Framework.
+' 
+' This source is subject to the Microsoft Public License.
+' See http://www.microsoft.com/en-us/openness/licenses.aspx#MPL.
+' All other rights reserved.
+' 
+' THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, 
+' EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED 
+' WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+'**************************************************************************/
+
+Imports System.ComponentModel.DataAnnotations.Schema
+Imports System.ComponentModel.DataAnnotations
+
+
+''' <summary>
+''' If we store the image in the blog, we will use this table to store 
+''' the info of image and uri of Blob.
+''' </summary>
+Public Class ImageInBlob
+    <Key()>
+    Public Property ImageId() As Int32
+    Public Property FileName() As String
+    Public Property ImageName() As String
+    Public Property Description() As String
+    Public Property BlobUri() As String
+End Class
+
+''' <summary>
+''' If we store the image in the SQL Azure, we will use this table to 
+''' store the info of the image.
+''' </summary>
+Public Class ImageInSQLAzure
+    <Key()>
+    Public Property ImageId() As Int32
+    Public Property FileName() As String
+    Public Property ImageName() As String
+    Public Property Description() As String
+End Class
+
+''' <summary>
+''' If we store the image in the SQL Azure we will use this table to 
+''' store the data of the image.
+''' </summary>
+Public Class ImagesTable
+    <Key()>
+    Public Property Id() As Int32
+
+    <Column(TypeName:="image")>
+    Public Property ImageData() As Byte()
+
+    Public Property ImageId() As Int32
+    <ForeignKey("ImageId")>
+    Public Property ImageInfo() As ImageInSQLAzure
+End Class
