af3fadfef4c7cfb7925160ac095540fc37955031
TerryFei <v-tiafe@microsoft.com>
2018-06-12 12:26:23 +0800

    Push project Stop postback in javascript

---
+++ OneCodeTeam/Set-value-to-IN-Parameter-of-SqlDataSource-20398/[C#]-Set value to IN Parameter of SqlDataSource/C#/CSASPNETInParameterOfSql/Default.aspx.cs
@@ -0,0 +1,75 @@
+﻿/****************************** Module Header ******************************\
+* Module Name:    Default.aspx.cs
+* Project:        CSASPNETInParameterOfSql
+* Copyright (c) Microsoft Corporation
+*
+* This sample code will demonstrate how to set value to “IN Parameter” of SqlDataSource
+* 
+* This source is subject to the Microsoft Public License.
+* See http://www.microsoft.com/en-us/openness/licenses.aspx#MPL.
+* All other rights reserved.
+* 
+* THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, 
+* EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED 
+* WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+\*****************************************************************************/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CSASPNETInParameterOfSql
+{
+    public partial class _Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// set the id dynamically
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        protected void btnDynamicShow_Click(object sender, EventArgs e)
+        {
+            // list of selected id
+            List<string> productIDs = new List<string>();
+
+            foreach (ListItem li in CheckBoxList1.Items)
+            {
+                if (li.Selected)
+                {
+                    productIDs.Add(li.Value);
+                }
+            }
+
+            if (productIDs.Count > 0)
+            {
+                SqlDataSource1.SelectCommand = String.Format(
+              "SELECT [Id], [Name] FROM [Test] WHERE ([Id] IN ({0}))",
+              String.Join(", ", productIDs.ToArray()));
+            }
+        }
+
+        /// <summary>
+        /// set the id static
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        protected void btnStaticShow_Click(object sender, EventArgs e)
+        {
+            // list of selected id
+            string s = "1,2,3,4,5";
+            string[] strings = s.Split(',');
+
+            SqlDataSource1.SelectCommand = String.Format(
+                "SELECT [Id], [Name] FROM [Test] WHERE ([Id] IN ({0}))",
+                String.Join(", ", strings.ToArray()));
+        }
+    }
+}
---
+++ OneCodeTeam/Stop-postback-in-javascript-20427/[VB]-Stop postback in javascript/VB/VBASPNETStopPostbackInJS/StopPostBack1.aspx.vb
@@ -0,0 +1,31 @@
+﻿'***************************** Module Header ******************************
+' Module Name: StopPostBack1.aspx.vb
+' Project:     VBASPNETStopPostbackInJS
+' Copyright (c) Microsoft Corporation
+'
+' This page uses a server button control's onClientClick event check to
+' stop or continue postbacks event.
+' 
+' This source is subject to the Microsoft Public License.
+' See http://www.microsoft.com/en-us/openness/licenses.aspx#MPL.
+' All other rights reserved.
+' 
+' THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, 
+' EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED 
+' WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+'*****************************************************************************
+
+
+
+Public Class StopPostBack1
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+
+    End Sub
+
+    Protected Sub btnCausePostback_Click(ByVal sender As Object, ByVal e As EventArgs) Handles btnCausePostback.Click
+        ' Postbacks code
+        textDisplay.Value += "  This is a server click"
+    End Sub
+End Class
