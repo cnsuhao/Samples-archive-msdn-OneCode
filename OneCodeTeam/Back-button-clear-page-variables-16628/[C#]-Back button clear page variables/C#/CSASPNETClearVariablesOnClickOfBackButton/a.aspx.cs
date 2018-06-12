6c9c461f8f308106e55ec1b9c7d315b5c7f66363
TerryFei <v-tiafe@microsoft.com>
2018-06-12 11:43:36 +0800

    Push project Back button clear page variables

---
+++ OneCodeTeam/Back-button-clear-page-variables-16628/[C#]-Back button clear page variables/C#/CSASPNETClearVariablesOnClickOfBackButton/a.aspx.cs
@@ -0,0 +1,44 @@
+﻿/****************************** Module Header ******************************\
+* Module Name:    a.aspx.cs
+* Project:        CSASPNETClearVariablesOnClickOfBackButton
+* Copyright (c) Microsoft Corporation
+*
+*  The CSASPNETClearVariablesOnClickOfBackButton sample demonstrates how to clear variables on click of Back button to previous page on browser
+* 
+* This source is subject to the Microsoft Public License.
+* See http://www.microsoft.com/en-us/openness/resources/licenses.aspx#MPL.
+* All other rights reserved.
+* 
+* THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, 
+* EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED 
+* WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+\*****************************************************************************/
+
+using System;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CSASPNETClearVariablesOnClickOfBackButton
+{
+    public partial class a : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // Clear out the session variable.
+            Response.Cache.SetCacheability(HttpCacheability.NoCache);
+            Response.Cache.SetExpires(DateTime.UtcNow.AddHours(-1));
+            Response.Cache.SetNoStore();
+        }
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(tbName.Text.Trim()) && !string.IsNullOrEmpty(tbPwd.Text.Trim()))
+            {
+                Session["uName"] = tbName.Text;
+                Session["uPwd"] = tbPwd.Text;
+                Response.Redirect("b.aspx");
+            }
+        }
+    }
+}
---
+++ OneCodeTeam/Back-button-clear-page-variables-16628/[C#]-Back button clear page variables/C#/CSASPNETClearVariablesOnClickOfBackButton/b.aspx.cs
@@ -0,0 +1,39 @@
+﻿/****************************** Module Header ******************************\
+* Module Name:    b.aspx.cs
+* Project:        CSASPNETClearVariablesOnClickOfBackButton
+* Copyright (c) Microsoft Corporation
+*
+* A page will show the data enter in a.aspx.
+* 
+* This source is subject to the Microsoft Public License.
+* See http://www.microsoft.com/en-us/openness/resources/licenses.aspx#MPL.
+* All other rights reserved.
+* 
+* THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, 
+* EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED 
+* WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+\*****************************************************************************/
+
+using System;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CSASPNETClearVariablesOnClickOfBackButton
+{
+    public partial class b : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {           
+            if (!string.IsNullOrEmpty(Session["uName"] as string))
+            {
+                Response.Write(Session["uName"].ToString());
+            }
+            Response.Write("<br/>");
+            if (!string.IsNullOrEmpty(Session["uPwd"] as string))
+            {
+                Response.Write(Session["uPwd"].ToString());
+            }
+        }
+    }
+}
