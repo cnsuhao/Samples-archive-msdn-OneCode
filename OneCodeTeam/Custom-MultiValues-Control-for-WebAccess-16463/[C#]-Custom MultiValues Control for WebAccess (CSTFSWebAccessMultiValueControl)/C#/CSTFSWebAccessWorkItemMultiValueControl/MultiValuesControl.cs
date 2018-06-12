3e08a01d16183683d97228512582833c38907324
TerryFei <v-tiafe@microsoft.com>
2018-06-12 11:30:56 +0800

    Push project Custom MultiValues Control for WebAccess (CSTFSWebAccessMultiValueControl)

---
+++ OneCodeTeam/Custom-MultiValues-Control-for-WebAccess-16463/[C#]-Custom MultiValues Control for WebAccess (CSTFSWebAccessMultiValueControl)/C#/CSTFSWebAccessWorkItemMultiValueControl/MultiValuesControl.cs
@@ -0,0 +1,175 @@
+﻿/****************************** Module Header ******************************\
+ * Module Name:   MultiValuesControl.cs
+ * Project:       CSTFSWebAccessWorkItemMultiValueControl
+ * Copyright (c) Microsoft Corporation.
+ * 
+ * This class inherits the BaseWITControl and it uses a list of check boxes to 
+ * represent a workitem field.
+ * 
+ * This source is subject to the Microsoft Public License.
+ * See http://www.microsoft.com/en-us/openness/licenses.aspx#MPL
+ * All other rights reserved.
+ * 
+ * THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, 
+ * EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED 
+ * WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+\***************************************************************************/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Microsoft.TeamFoundation.WebAccess.WorkItemTracking.Controls;
+using System.Drawing;
+using Microsoft.TeamFoundation.WebAccess.Controls;
+
+// Defines the metadata attribute that enables an embedded resource in an assembly.
+[assembly: WebResource("CSTFSWebAccessWorkItemMultiValueControl.MultiValuesControl.js",
+    "application/x-javascript")]
+
+namespace CSTFSWebAccessWorkItemMultiValueControl
+{
+    /// <summary>
+    /// This class inherits BaseWorkItemWebControl which implements most methods of the 
+    /// IWorkItemWebControl interface.
+    /// </summary>
+    [ToolboxData("<{0}:MultiValuesControl runat=server></{0}:MultiValuesControl>")]
+    public class MultiValuesControl : BaseWorkItemWebControl
+    {
+
+        // This char is used to split the options.
+        const char splitCharactor = ';';
+
+        // A list of CheckBoxes which represent the options.
+        private CheckBoxList lstOptions;
+
+        public MultiValuesControl()
+            : base(HtmlTextWriterTag.Div)
+        { }
+
+        #region IWorkItemWebControl
+
+        /// <summary>
+        /// Clear the value in the checkboxes.
+        /// </summary>
+        public override void Clear()
+        {
+            this.EnsureChildControls();
+            for (int i = 0; i < this.lstOptions.Items.Count; i++)
+            {
+                this.lstOptions.Items[i].Selected = false;
+            }
+        }
+
+        /// <summary>
+        /// Control is requested to flush any data to workitem object. 
+        /// Combine the selected options to a string, and then set it as the 
+        /// field value.
+        /// </summary>
+        public override void FlushToDatasource()
+        {
+            this.EnsureChildControls();
+
+            StringBuilder value = new StringBuilder();
+            for (int i = 0; i < this.lstOptions.Items.Count; i++)
+            {
+                if (this.lstOptions.Items[i].Selected)
+                {
+                    value.AppendFormat("{0}{1}", this.lstOptions.Items[i],
+                        splitCharactor);
+                }
+            }
+            this.SetFieldValue(value.ToString());
+        }
+
+        ///<summary>
+        /// Asks control to invalidate the contents and redraw.
+        /// </summary>
+        public override void InvalidateDatasource()
+        {
+            this.EnsureChildControls();
+            this.Clear();
+
+            this.lstOptions.BackColor = Color.White;
+
+            for (int i = 0; i < this.lstOptions.Items.Count; i++)
+            {
+                bool checkedState = (this.Field.Value as string).Contains(
+                    string.Format("{0}{1}", this.lstOptions.Items[i].Text, splitCharactor));
+                this.lstOptions.Items[i].Selected = checkedState;
+            }
+        }
+        #endregion
+
+        #region  Overrides methods
+
+        /// <summary>
+        /// Create the child controls.
+        /// </summary>
+        protected override void CreateChildControls()
+        {
+            this.Controls.Clear();
+            this.lstOptions = new CheckBoxList();
+            this.lstOptions.ID = "val";
+            if (this.HasValidField && this.Field.HasAllowedValuesList)
+            {
+                foreach (var value in this.Field.AllowedValues)
+                {
+
+                    ListItem item = new ListItem
+                    {
+                        Text = value as string,
+                        Value = value as string
+                    };
+
+                    item.Attributes.Add("title", value as string);
+
+                    this.lstOptions.Items.Add(item);
+                }
+            }
+            this.Controls.Add(this.lstOptions);
+        }
+
+        /// <summary>
+        /// Initialize this control.
+        /// </summary>
+        public override void InitializeControl()
+        {
+            base.InitializeControl();
+            this.EnsureChildControls();
+        }
+
+        /// <summary>
+        /// Register the client script.
+        /// 1. CSTFSWebAccessWorkItemMultiValueControl.MultiValuesControl.js
+        /// 2. Use multiValuesControl method in CSTFSWebAccessWorkItemMultiValueControl.MultiValuesControl.js
+        ///    to generate a client object.
+        /// </summary>
+        /// <param name="e"></param>
+        protected override void OnPreRender(EventArgs e)
+        {
+            base.OnPreRender(e);
+            ScriptManager.RegisterClientScriptResource(
+                this, typeof(MultiValuesControl),
+                "CSTFSWebAccessWorkItemMultiValueControl.MultiValuesControl.js");
+
+            string clientID = this.lstOptions.ClientID;
+
+            ScriptHelper.RegisterObjectScript(
+                this,
+                "multiValuesControl",
+                new object[] {
+                    base.ClientEditorObjectId, 
+                    base.WorkItemFieldName,                   
+                    base.ControlId, 
+                    clientID,
+                    base.ReadOnly
+                });
+        }
+        #endregion
+    }
+}
---
+++ OneCodeTeam/Custom-MultiValues-Control-for-WebAccess-16463/[C#]-Custom MultiValues Control for WebAccess (CSTFSWebAccessMultiValueControl)/C#/CSTFSWebAccessWorkItemMultiValueControl/MultiValuesControl.js
@@ -0,0 +1,125 @@
+﻿/// <reference name="MicrosoftAjax.js" />
+/// <reference path="Utils.js" />
+/// <reference path="EditWorkItem.js" />
+
+
+// The constructor of the multiValuesControl.
+
+function multiValuesControl(id,
+                      workItemEditorObjectId,
+                      fieldName,
+                      controlId,
+                      innerControlId,
+                      readOnly) {
+
+    // The m_workItemEditor represents the workItemEditor object in client side.
+    // It supply methods to 
+    //     1. Get field value.
+    //     2. Update field value.
+    //     3. Refresh all field.
+    //     4. Other services. 
+    // We can use $getObject(m_workItemEditorObjectId) to get it later.
+    this.m_workItemEditorObjectId = workItemEditorObjectId;
+    this.m_workItemEditor = null;
+
+    // The field name.
+    this.m_fieldName = fieldName;
+
+    // The ID of this control.
+    // We can use $getObject(controlId) to get this object.
+    this.m_controlId = controlId;
+
+    // The id of the lstOptions.
+    this.m_innerControlId = innerControlId;
+
+    // The client html element of pnlContainer.
+    this.m_control = null;
+    this.m_readOnly = readOnly;
+
+    // Array of checkboxes
+    this.m_options = null;
+
+}
+
+// This method will be called by m_workItemEditor.
+multiValuesControl.prototype.init = function multiValuesControl$init() {
+    this.m_workItemEditor = $getObject(this.m_workItemEditorObjectId);
+
+    // Attach a event handler to the editorReady event of the workItemEditor.
+    // The workItemEditor has other event like 
+    //     1. synchronize.
+    //     2. serverRoundtripStart.
+    //     3. fieldChange.
+    //     4. updateField-complete.
+    //     5. beforeSave.
+    //     6. serverRoundtripEnd.
+    //     7. add-link.
+    // You can find all the definition in EditWorkItem.js
+    this.m_workItemEditor.attachEvent("editorReady", Function.createDelegate(this, this.onEditorReady));
+
+    this.m_control = $getObject(this.m_innerControlId);
+    this.m_options = JsUtility.getElementsByTagName(this.m_control, "input");
+    for (i = 0; i < this.m_options.length; i++) {
+        JsUtility.attachEvent(this.m_options[i], "change",
+            Function.createDelegate(this, this.onCheckboxChanged), true);
+    }
+
+}
+
+// Initialize the control.
+multiValuesControl.prototype.onEditorReady = function multiValuesControl$onEditorReady() {
+    this.reinitCheckboxList();
+}
+
+// Initialize the control.
+multiValuesControl.prototype.reinitCheckboxList = function multiValuesControl$reinitCheckboxList() {
+
+    // Get the field object.
+    var _field = this.m_workItemEditor.getField(this.m_fieldName);
+
+    if (_field) {
+        // Determine whether the field is readonly/enabled.
+        var _fieldReadOnly = !_field.IsEditable || _field.IsComputed;
+        var _controlReadOnly = this.m_readOnly || _fieldReadOnly;
+        var _enabled = !(this.m_workItemEditor.ReadOnly || _controlReadOnly);
+        var _required = _enabled ? _field.IsRequired : false;
+
+        for (i = 0; i < this.m_options.length; i++) {
+            if (_enabled == true) {
+                this.m_options[i].removeAttribute("disabled");
+            }
+            else {
+                this.m_options[i].setAttribute("disabled", "disabled");
+            }
+
+            if (_field.Value != null && _field.Value.indexOf(this.m_options[i].parentElement.title + ";") >= 0) {
+                this.m_options[i].checked = true;
+            }
+            else {
+                this.m_options[i].checked = false;
+            }
+        }
+    }
+}
+
+// Handle the change event of the checkbox, and then update the field.
+multiValuesControl.prototype.onCheckboxChanged = function multiValuesControl$onCheckboxChanged(e) {
+
+    var _value = new Sys.StringBuilder();
+
+    var _completedFieldValue = 1;
+
+    for (i = 0; i < this.m_options.length; i++) {
+        if (this.m_options[i].checked == true) {
+            _value.append(this.m_options[i].parentElement.title + ";");
+        }
+        else {
+            _completedFieldValue = 0;
+        }
+    }
+
+    var _isValid = true;
+    var _lastError = "";
+
+    this.m_workItemEditor.updateField(this, this.m_fieldName, _value.toString(), _isValid, _lastError, this.m_controlId);
+}
