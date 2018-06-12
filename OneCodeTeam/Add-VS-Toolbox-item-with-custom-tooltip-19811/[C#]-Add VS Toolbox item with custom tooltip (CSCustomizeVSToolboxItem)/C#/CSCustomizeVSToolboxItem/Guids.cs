487d1634169d62a344d686d98140b8965739b52d
TerryFei <v-tiafe@microsoft.com>
2018-06-12 12:12:53 +0800

    Push project Add VS Toolbox item with custom tooltip (CSCustomizeVSToolboxItem)

---
+++ OneCodeTeam/Add-VS-Toolbox-item-with-custom-tooltip-19811/[C#]-Add VS Toolbox item with custom tooltip (CSCustomizeVSToolboxItem)/C#/CSCustomizeVSToolboxItem/Guids.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Microsoft.CSCustomizeVSToolboxItem
+{
+    static class GuidList
+    {
+        public const string guidCSCustomizeVSToolboxItemPkgString = "e4d6bd4f-2378-4002-a15a-b4f3ef0eaaad";
+        public const string guidCSCustomizeVSToolboxItemCmdSetString = "25938962-8d18-48a1-b172-3cfd7b982763";
+
+        public static readonly Guid guidCSCustomizeVSToolboxItemCmdSet = new Guid(guidCSCustomizeVSToolboxItemCmdSetString);
+    };
+}
