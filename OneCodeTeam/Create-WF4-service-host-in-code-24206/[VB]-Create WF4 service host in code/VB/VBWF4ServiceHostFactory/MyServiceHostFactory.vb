341b2d1c02d8219a187fec591a7d1bf0a919db52
TerryFei <v-tiafe@microsoft.com>
2018-06-12 13:15:51 +0800

    Push project Create WF4 service host in code

---
+++ OneCodeTeam/Create-WF4-service-host-in-code-24206/[VB]-Create WF4 service host in code/VB/VBWF4ServiceHostFactory/MyServiceHostFactory.vb
@@ -0,0 +1,30 @@
+﻿'****************************** Module Header ******************************\
+' Module Name:  MyServiceHostFactory.vb
+' Project:		VBWF4ServiceHostFactory
+' Copyright (c) Microsoft Corporation.
+' 
+' This source is subject to the Microsoft Public License.
+' See http://www.microsoft.com/en-us/openness/licenses.aspx#MPL.
+' All other rights reserved.
+' 
+' THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, 
+' EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED 
+' WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+'***************************************************************************/
+Imports System.Activities.DurableInstancing
+Imports System.ServiceModel.Activities
+Imports System.ServiceModel.Activities.Activation
+
+Public Class MyServiceHostFactory
+    Inherits WorkflowServiceHostFactory
+    Protected Overrides Function CreateWorkflowServiceHost(service As WorkflowService, baseAddresses As Uri()) As WorkflowServiceHost
+        Dim host As WorkflowServiceHost = MyBase.CreateWorkflowServiceHost(service, baseAddresses)
+        Dim connectionString As String = ConfigurationManager.AppSettings("SqlWF4PersistenceConnectionString").ToString()
+        Dim instanceStore As New SqlWorkflowInstanceStore(connectionString)
+        instanceStore.InstanceCompletionAction = InstanceCompletionAction.DeleteNothing
+        host.DurableInstancingOptions.InstanceStore = instanceStore
+        Return host
+    End Function
+End Class
+
+
