d0593f98b9bd1e7f3ed9498e61979f63a978118d
TerryFei <v-tiafe@microsoft.com>
2018-06-12 11:34:12 +0800

    Push project Keep AutoComplete List Open (ASPNETKeepAutoCompleteListOpen)

---
+++ OneCodeTeam/Keep-AutoComplete-List-Open-16487/[C#]-Keep AutoComplete List Open (ASPNETKeepAutoCompleteListOpen)/C#/CSASPNETKeepAutoCompleteListOpen/Default.aspx.cs
@@ -0,0 +1,33 @@
+﻿/****************************** Module Header ******************************\
+* Module Name:    Default.aspx.cs
+* Project:        CSASPNETKeepAutoCompleteListOpen
+* Copyright (c) Microsoft Corporation
+*
+* The project shows how to keep AutoComplete List Open, and select multiple options.
+* 
+* This source is subject to the Microsoft Public License.
+* See http://www.microsoft.com/en-us/openness/licenses.aspx#MPL.
+* All other rights reserved.
+* 
+* THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, 
+* EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED 
+* WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+\*****************************************************************************/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CSASPNETKeepAutoCompleteListOpen
+{
+    public partial class _Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
