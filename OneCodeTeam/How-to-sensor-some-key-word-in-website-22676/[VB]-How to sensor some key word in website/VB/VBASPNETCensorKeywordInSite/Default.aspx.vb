cbd112af0ae081e229f36f0e402fe39b4d7a1155
TerryFei <v-tiafe@microsoft.com>
2018-06-12 12:58:49 +0800

    Push project How to sensor some key word in website

---
+++ OneCodeTeam/How-to-sensor-some-key-word-in-website-22676/[VB]-How to sensor some key word in website/VB/VBASPNETCensorKeywordInSite/Default.aspx.vb
@@ -0,0 +1,90 @@
+﻿'****************************** Module Header ******************************\
+' Module Name:  Default.aspx.vb
+' Project:      VBASPNETSensorKeywordInSite
+' Copyright (c) Microsoft Corporation
+'
+' The project illustrates how to block some indecent words in website
+' 
+' This source is subject to the Microsoft Public License.
+' See http://www.microsoft.com/en-us/openness/licenses.aspx#MPL.
+' All other rights reserved.
+' 
+' THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, 
+' EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED 
+' WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+'***************************************************************************/
+Imports System.Data.SqlClient
+
+Partial Public Class _Default
+    Inherits System.Web.UI.Page
+
+    'Sql Connection
+    Private Shared conn As New SqlConnection("Data Source=.\SQLEXPRESS;AttachDbFilename=|DataDirectory|\Sample.mdf;Integrated Security=True;User Instance=True")
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs) Handles Me.Load
+
+
+    End Sub
+
+    Protected Sub btnEnter_Click(ByVal sender As Object, ByVal e As EventArgs)
+        Dim str As String = tbText.Text
+        str = str.Trim()
+        'Remove the spaces and format symbols in the data
+        Dim str1 As String = str.Replace(" ", "")
+
+        Dim isBool As Boolean = ValidByReg(str1)
+
+        If isBool Then
+            ltrMsg.Text = str
+        Else
+            ltrMsg.Text = ReplacDirty(str)
+        End If
+    End Sub
+
+    'The list of KeyBlack，such as：dirtyStr1|dirtyStr2|dirtyStr3
+    Public Shared dirtyStr As String = ""
+
+    Public Function ReplacDirty(ByVal str As String) As String
+        dirtyStr = ReadDic()
+        Try
+            str = Regex.Replace(str, "" & dirtyStr & "", "xxxxx")
+            ' Syntax error in the regular expression
+        Catch ex As ArgumentException
+        End Try
+        Return str
+    End Function
+
+
+    Private Function ReadDic() As String
+        Dim input As [String] = ""
+
+        'Query string
+        Dim queryString As String = "SELECT [Name] FROM [WordBlack]"
+        'set query string
+        Dim command As New SqlCommand(queryString, conn)
+        'Open connection
+        conn.Open()
+        Dim reader As SqlDataReader = command.ExecuteReader()
+
+        If reader.HasRows Then
+            While reader.Read()
+                input += "|" & TryCast(reader("Name"), String).Trim()
+            End While
+            input = input.Substring(1)
+        End If
+        reader.Close()
+        'Close connection
+        conn.Close()
+        Return input
+
+    End Function
+
+    Public Function ValidByReg(ByVal str As String) As Boolean
+        dirtyStr = ReadDic()
+        'Regular expression used to detect dirty dictionary
+        Dim validateReg As New Regex("^((?!" & dirtyStr & ").(?<!" & dirtyStr & "))*$", RegexOptions.Compiled Or RegexOptions.ExplicitCapture)
+
+        Return validateReg.IsMatch(str)
+    End Function
+
+End Class
---
+++ OneCodeTeam/How-to-sensor-some-key-word-in-website-22676/[VB]-How to sensor some key word in website/VB/VBASPNETCensorKeywordInSite/My Project/AssemblyInfo.vb
@@ -0,0 +1,34 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+<Assembly: AssemblyTitle("VBASPNETCensorKeywordInSite")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("Microsoft Corporation")> 
+<Assembly: AssemblyProduct("VBASPNETCensorKeywordInSite")> 
+<Assembly: AssemblyCopyright("Copyright © Microsoft Corporation 2012")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("088e2ffe-c9f7-44e5-bf20-b77b7753269c")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
