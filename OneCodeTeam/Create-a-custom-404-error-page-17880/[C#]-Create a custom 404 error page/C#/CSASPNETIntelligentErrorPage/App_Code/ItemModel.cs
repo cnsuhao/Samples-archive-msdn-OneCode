e9bf772500240f00c8c86371a1cfdb4807d7f1aa
TerryFei <v-tiafe@microsoft.com>
2018-06-12 12:03:32 +0800

    Push project Create a custom 404 error page

---
+++ OneCodeTeam/Create-a-custom-404-error-page-17880/[C#]-Create a custom 404 error page/C#/CSASPNETIntelligentErrorPage/App_Code/ItemModel.cs
@@ -0,0 +1,52 @@
+﻿/****************************** Module Header ******************************\
+Module Name:  ItemModel.cs
+Project:      CSASPNETIntelligentErrorPage
+Copyright (c) Microsoft Corporation.
+ 
+The sample code demonstrates how to create the intelligent error page in 
+Asp.net application. In this sample, we add a custom 404 error page and find 
+the similar local resources, then display them in error page. In this way, 
+we will improve the user-experience, since users don’t need to face a boring 
+and helpless error page any more.
+
+This class is the model of url path.
+ 
+This source is subject to the Microsoft Public License.
+See http://www.microsoft.com/en-us/openness/licenses.aspx#MPL
+All other rights reserved.
+ 
+THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, 
+EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED 
+WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+\***************************************************************************/
+
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CSASPNETIntelligentErrorPage
+{
+    public class ItemModel
+    {
+        public string Name
+        {
+            get;
+            set;
+        }
+
+        public string Category
+        {
+            get;
+            set;
+        }
+
+        public string Url
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ OneCodeTeam/Create-a-custom-404-error-page-17880/[C#]-Create a custom 404 error page/C#/CSASPNETIntelligentErrorPage/Relative Pages/Silverlight.aspx
@@ -0,0 +1,16 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Silverlight.aspx.cs" Inherits="CSASPNETIntelligentErrorPage.Relative_Pages.Silverlight" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    I am a Silverlight web page.
+    </div>
+    </form>
+</body>
+</html>
