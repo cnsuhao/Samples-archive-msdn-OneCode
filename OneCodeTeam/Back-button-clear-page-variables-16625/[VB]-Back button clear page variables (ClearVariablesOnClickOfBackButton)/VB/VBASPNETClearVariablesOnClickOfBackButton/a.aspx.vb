050bd5272f35585c5c1f3bf6f21da3ca64f3f7d3
TerryFei <v-tiafe@microsoft.com>
2018-06-12 11:43:12 +0800

    Push project Back button clear page variables (ClearVariablesOnClickOfBackButton)

---
+++ OneCodeTeam/Back-button-clear-page-variables-16625/[VB]-Back button clear page variables (ClearVariablesOnClickOfBackButton)/VB/VBASPNETClearVariablesOnClickOfBackButton/a.aspx.vb
@@ -0,0 +1,34 @@
+﻿'****************************** Module Header ******************************\
+' Module Name:    a.aspx.vb
+' Project:        VBASPNETClearVariablesOnClickOfBackButton
+' Copyright (c) Microsoft Corporation
+
+'  The VBASPNETClearVariablesOnClickOfBackButton sample demonstrates how to clear variables on click of Back button to previous page on browser
+
+' This source is subject to the Microsoft Public License.
+' See http://www.microsoft.com/en-us/openness/resources/licenses.aspx#MPL.
+' All other rights reserved.
+' 
+' THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, 
+' EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED 
+' WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+'***************************************************************************/
+
+Partial Public Class a
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        ' Clear out the session variable.
+        Response.Cache.SetCacheability(HttpCacheability.NoCache)
+        Response.Cache.SetExpires(DateTime.UtcNow.AddHours(-1))
+        Response.Cache.SetNoStore()
+    End Sub
+
+    Protected Sub btnSubmit_Click(ByVal sender As Object, ByVal e As EventArgs) Handles btnSubmit.Click
+        If Not String.IsNullOrEmpty(tbName.Text.Trim()) AndAlso Not String.IsNullOrEmpty(tbPwd.Text.Trim()) Then
+            Session("uName") = tbName.Text
+            Session("uPwd") = tbPwd.Text
+            Response.Redirect("b.aspx")
+        End If
+    End Sub
+End Class
---
+++ OneCodeTeam/Back-button-clear-page-variables-16625/[VB]-Back button clear page variables (ClearVariablesOnClickOfBackButton)/VB/VBASPNETClearVariablesOnClickOfBackButton/b.aspx.vb
@@ -0,0 +1,30 @@
+﻿'****************************** Module Header ******************************\
+' Module Name:    b.aspx.vb
+' Project:        VBASPNETClearVariablesOnClickOfBackButton
+' Copyright (c) Microsoft Corporation
+
+' A page will show the data enter in a.aspx.
+
+' This source is subject to the Microsoft Public License.
+' See http://www.microsoft.com/en-us/openness/resources/licenses.aspx#MPL.
+' All other rights reserved.
+' 
+' THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, 
+' EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED 
+' WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+'***************************************************************************/
+
+Partial Public Class b
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        If Not String.IsNullOrEmpty(TryCast(Session("uName"), String)) Then
+            Response.Write(Session("uName").ToString())
+        End If
+        Response.Write("<br/>")
+        If Not String.IsNullOrEmpty(TryCast(Session("uPwd"), String)) Then
+            Response.Write(Session("uPwd").ToString())
+        End If
+    End Sub
+
+End Class
