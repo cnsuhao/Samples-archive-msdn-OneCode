473b0ef160da63d44401edd7552312ec0fcac478
TerryFei <v-tiafe@microsoft.com>
2018-06-12 11:59:40 +0800

    Push project Check whether the page is re-fresh (CSASPNETCheckPageRefresh)

---
+++ OneCodeTeam/Check-whether-the-page-is-re-fresh-17854/[C#]-Check whether the page is re-fresh (CSASPNETCheckPageRefresh)/C#/CSASPNETCheckPageRefresh/Default.aspx
@@ -0,0 +1,42 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="CSASPNETCheckPageRefresh._Default" %>
+
+<%@ Register Src="CheckRefreshUserControl.ascx" TagName="CheckRefreshUserControl"
+    TagPrefix="uc1" %>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <uc1:CheckRefreshUserControl ID="CheckRefreshUserControl1" runat="server" />
+        <table>
+            <caption>
+                Order Pay</caption>
+            <tr>
+                <td>
+                    OrderNo
+                </td>
+                <td>
+                    20120409001
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    Money
+                </td>
+                <td>
+                    $1
+                </td>
+            </tr>
+            <tr>
+                <td colspan="2">
+                    <asp:Button ID="btnSubmit" runat="server" OnClick="btnSubmit_Click" Text="Pay" />
+                </td>
+            </tr>
+        </table>
+    </div>
+    </form>
+</body>
+</html>
---
+++ OneCodeTeam/Check-whether-the-page-is-re-fresh-17854/[C#]-Check whether the page is re-fresh (CSASPNETCheckPageRefresh)/C#/CSASPNETCheckPageRefresh/Default.aspx.cs
@@ -0,0 +1,77 @@
+﻿/****************************** Module Header ******************************\
+* Module Name:    Default.aspx.cs
+* Project:        CSASPNETCheckPageRefresh
+* Copyright (c) Microsoft Corporation
+*
+* The project illustrates how to check whether the page is refreshed or not.
+* 
+* This source is subject to the Microsoft Public License.
+* See http://www.microsoft.com/en-us/openness/licenses.aspx#MPL.
+* All other rights reserved.
+* 
+* THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, 
+* EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED 
+* WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+\*****************************************************************************/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CSASPNETCheckPageRefresh
+{
+    public partial class _Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (IsNotRefresh())
+            {
+                WriteMsg("Page is not refreshed.");
+                // Do your logic code here
+            }
+            else
+            {
+                WriteMsg("Page is refreshed.");
+                // Do your logic code here
+            }
+         
+        }
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            if (IsNotRefresh())
+            {
+                WriteMsg("Page is not refreshed.");
+                // Do your logic code here
+            }
+        }
+
+        /// <summary>
+        /// Output some message
+        /// </summary>
+        /// <param name="strMsg"></param>
+        private void WriteMsg(string strMsg)
+        {
+            Response.Clear();
+            Response.Write(strMsg);
+        }
+
+        /// <summary>
+        /// Determine whether is refresh or not
+        /// </summary>
+        /// <returns></returns>
+        private bool IsNotRefresh()
+        {
+            bool isNotRefresh = true;
+
+            // User Control
+            CheckRefreshUserControl cruc = this.FindControl("CheckRefreshUserControl1") as CheckRefreshUserControl;            
+            // Result
+            isNotRefresh = cruc.ReFreshCheck == false ? true : false;
+            return isNotRefresh;
+        }
+    }
+}
