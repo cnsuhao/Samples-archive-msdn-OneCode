2db90136b40aa8686bf7c24eb471d663ab7ac0bb
TerryFei <v-tiafe@microsoft.com>
2018-06-12 12:49:27 +0800

    Push project How to add images to Powerpoint dynamically using OpenXML

---
+++ OneCodeTeam/How-to-add-to-Powerpoint-dynamically-using-OpenXML-22619/[VB]-How to add images to Powerpoint dynamically using OpenXML/VB/VBOpenXmlInsertImageToPPT/InsertImage.vb
@@ -0,0 +1,169 @@
+﻿'***************************** Module Header ******************************\
+' Module Name:  InsertImage.vb
+' Project:      VBOpenXmlInsertImageToPPT
+' Copyright(c)  Microsoft Corporation.
+' 
+' The Class is used to Insert Image into PowerPoint using Open XML SDK.
+' 
+' This source is subject to the Microsoft Public License.
+' See http://www.microsoft.com/en-us/openness/licenses.aspx.
+' All other rights reserved.
+' 
+' THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, 
+' EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED 
+' WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+'**************************************************************************/
+
+Imports System.IO
+Imports System.Linq
+Imports DocumentFormat.OpenXml
+Imports DocumentFormat.OpenXml.Drawing
+Imports DocumentFormat.OpenXml.Office2010.Drawing
+Imports DocumentFormat.OpenXml.Packaging
+Imports DocumentFormat.OpenXml.Presentation
+Imports A = DocumentFormat.OpenXml.Drawing
+Imports P = DocumentFormat.OpenXml.Presentation
+Public Class InsertImage
+    ''' <summary>
+    ''' Insert a new Slide into PowerPoint
+    ''' </summary>
+    ''' <param name="presentationPart">Presentation Part</param>
+    ''' <param name="layoutName">Layout of the new Slide</param>
+    ''' <returns>Slide Instance</returns>
+    Public Function InsertSlide(presentationPart As PresentationPart, layoutName As String) As Slide
+        Dim slideId As UInt32 = 256UI
+
+        ' Get the Slide Id collection of the presentation document
+        Dim slideIdList = presentationPart.Presentation.SlideIdList
+
+        If slideIdList Is Nothing Then
+            Throw New NullReferenceException("The number of slide is empty, please select a ppt with a slide at least again")
+        End If
+
+        slideId += Convert.ToUInt32(slideIdList.Count())
+
+        ' Creates a Slide instance and adds its children.
+        Dim slide As New Slide(New CommonSlideData(New ShapeTree()))
+
+        Dim slidePart As SlidePart = presentationPart.AddNewPart(Of SlidePart)()
+        slide.Save(slidePart)
+
+        ' Get SlideMasterPart and SlideLayoutPart from the existing Presentation Part
+        Dim slideMasterPart As SlideMasterPart = presentationPart.SlideMasterParts.First()
+        Dim slideLayoutPart As SlideLayoutPart = slideMasterPart.SlideLayoutParts.SingleOrDefault(Function(sl) sl.SlideLayout.CommonSlideData.Name.Value.Equals(layoutName, StringComparison.OrdinalIgnoreCase))
+        If slideLayoutPart Is Nothing Then
+            Throw New Exception("The slide layout " & layoutName & " is not found")
+        End If
+
+        slidePart.AddPart(Of SlideLayoutPart)(slideLayoutPart)
+
+        slidePart.Slide.CommonSlideData = DirectCast(slideMasterPart.SlideLayoutParts.SingleOrDefault(Function(sl) sl.SlideLayout.CommonSlideData.Name.Value.Equals(layoutName)).SlideLayout.CommonSlideData.Clone(), CommonSlideData)
+
+        ' Create SlideId instance and Set property
+        Dim newSlideId As SlideId = presentationPart.Presentation.SlideIdList.AppendChild(Of SlideId)(New SlideId())
+        newSlideId.Id = slideId
+        newSlideId.RelationshipId = presentationPart.GetIdOfPart(slidePart)
+
+        Return GetSlideByRelationShipId(presentationPart, newSlideId.RelationshipId)
+    End Function
+
+    ''' <summary>
+    ''' Get Slide By RelationShip ID
+    ''' </summary>
+    ''' <param name="presentationPart">Presentation Part</param>
+    ''' <param name="relationshipId">Relationship ID</param>
+    ''' <returns>Slide Object</returns>
+    Private Shared Function GetSlideByRelationShipId(presentationPart As PresentationPart, relationshipId As StringValue) As Slide
+        ' Get Slide object by Relationship ID
+        Dim slidePart As SlidePart = TryCast(presentationPart.GetPartById(relationshipId), SlidePart)
+        If slidePart IsNot Nothing Then
+            Return slidePart.Slide
+        Else
+            Return Nothing
+        End If
+    End Function
+
+    ''' <summary>
+    ''' Insert Image into Slide
+    ''' </summary>
+    ''' <param name="imagePath">Image Path</param>
+    ''' <param name="imageExt">Image Extension</param>
+    Public Sub InsertImageInLastSlide(slide As Slide, imagePath As String, imageExt As String)
+        ' Creates a Picture instance and adds its children.
+        Dim picture As New P.Picture()
+        Dim embedId As String = String.Empty
+        embedId = "rId" & (slide.Elements(Of P.Picture)().Count() + 915).ToString()
+        Dim nonVisualPictureProperties As New P.NonVisualPictureProperties(New P.NonVisualDrawingProperties() With { _
+         .Id = 4, _
+         .Name = "Picture 5" _
+        }, New P.NonVisualPictureDrawingProperties(New A.PictureLocks() With { _
+         .NoChangeAspect = True _
+        }), New ApplicationNonVisualDrawingProperties())
+
+        Dim blipFill As New P.BlipFill()
+        Dim blip As New Blip() With { _
+         .Embed = embedId _
+        }
+
+        ' Creates a BlipExtensionList instance and adds its children
+        Dim blipExtensionList As New BlipExtensionList()
+        Dim blipExtension As New BlipExtension() With { _
+         .Uri = "{28A0092B-C50C-407E-A947-70E740481C1C}" _
+        }
+
+        Dim useLocalDpi As New UseLocalDpi() With { _
+         .Val = False _
+        }
+        useLocalDpi.AddNamespaceDeclaration("a14", "http://schemas.microsoft.com/office/drawing/2010/main")
+
+        blipExtension.Append(useLocalDpi)
+        blipExtensionList.Append(blipExtension)
+        blip.Append(blipExtensionList)
+
+        Dim stretch As New Stretch()
+        Dim fillRectangle As New FillRectangle()
+        stretch.Append(fillRectangle)
+
+        blipFill.Append(blip)
+        blipFill.Append(stretch)
+
+        ' Creates a ShapeProperties instance and adds its children.
+        Dim shapeProperties As New P.ShapeProperties()
+
+        Dim transform2D As New A.Transform2D()
+        Dim offset As New A.Offset() With { _
+          .X = 457200L, _
+          .Y = 1524000L _
+        }
+        Dim extents As New A.Extents() With { _
+          .Cx = 8229600L, _
+          .Cy = 5029200L _
+        }
+
+        transform2D.Append(offset)
+        transform2D.Append(extents)
+
+        Dim presetGeometry As New A.PresetGeometry() With { _
+         .Preset = A.ShapeTypeValues.Rectangle _
+        }
+        Dim adjustValueList As New A.AdjustValueList()
+
+        presetGeometry.Append(adjustValueList)
+
+        shapeProperties.Append(transform2D)
+        shapeProperties.Append(presetGeometry)
+
+        picture.Append(nonVisualPictureProperties)
+        picture.Append(blipFill)
+        picture.Append(shapeProperties)
+
+        slide.CommonSlideData.ShapeTree.AppendChild(picture)
+
+        ' Generates content of imagePart.
+        Dim imagePart As ImagePart = slide.SlidePart.AddNewPart(Of ImagePart)(imageExt, embedId)
+        Dim fileStream As New FileStream(imagePath, FileMode.Open)
+        imagePart.FeedData(fileStream)
+        fileStream.Close()
+    End Sub
+
+End Class
---
+++ OneCodeTeam/How-to-add-to-Powerpoint-dynamically-using-OpenXML-22619/[VB]-How to add images to Powerpoint dynamically using OpenXML/VB/VBOpenXmlInsertImageToPPT/MainForm.vb
@@ -0,0 +1,100 @@
+﻿'***************************** Module Header ******************************\
+' Module Name:  MainForm.vb
+' Project:      VBOpenXmlInsertImageToPPT
+' Copyright(c)  Microsoft Corporation.
+' 
+' This is the main form of this application. 
+' It is used to initialize the UI and handle the events.
+' 
+' This source is subject to the Microsoft Public License.
+' See http://www.microsoft.com/en-us/openness/licenses.aspx.
+' All other rights reserved.
+' 
+' THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, 
+' EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED 
+' WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+'**************************************************************************/
+
+
+Imports System.IO
+Imports System.Windows.Forms
+Imports DocumentFormat.OpenXml.Packaging
+Imports DocumentFormat.OpenXml.Presentation
+
+Public Class MainForm
+
+    ''' <summary>
+    ''' Handle Click events of Open button
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub btnOpenPPt_Click(sender As System.Object, e As System.EventArgs) Handles btnOpenPPt.Click
+        Using dialog As New OpenFileDialog()
+            dialog.Filter = "PowerPoint document (*.pptx)|*.pptx"
+            dialog.InitialDirectory = Environment.CurrentDirectory
+
+            ' Retore the directory before closing
+            dialog.RestoreDirectory = True
+            If dialog.ShowDialog() = DialogResult.OK Then
+                Me.txbPPtPath.Text = dialog.FileName
+            End If
+        End Using
+
+    End Sub
+
+    ''' <summary>
+    ''' Handle Click events of Select button
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub btnSelectImg_Click(sender As System.Object, e As System.EventArgs) Handles btnSelectImg.Click
+        Using dialog As New OpenFileDialog()
+            dialog.Filter = "Pictures(*.jpg;*.png)|*.jpg;*.png"
+            dialog.InitialDirectory = Environment.CurrentDirectory
+
+            ' Retore the directory before closing
+            dialog.RestoreDirectory = True
+            If dialog.ShowDialog() = DialogResult.OK Then
+                Me.txbImagePath.Text = dialog.FileName
+            End If
+        End Using
+
+    End Sub
+
+    ''' <summary>
+    ''' Handle client event of Insert button 
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub btnInsert_Click(sender As System.Object, e As System.EventArgs) Handles btnInsert.Click
+        Dim pptFilePath As String = txbPPtPath.Text.Trim()
+        Dim imagefilePath As String = txbImagePath.Text.Trim()
+        Dim imageExt As String = Path.GetExtension(imagefilePath)
+        If imageExt.Equals("jpg", StringComparison.OrdinalIgnoreCase) Then
+            imageExt = "image/jpeg"
+        Else
+            imageExt = "image/png"
+        End If
+        Dim condition As Boolean = String.IsNullOrEmpty(pptFilePath) OrElse Not File.Exists(pptFilePath) OrElse String.IsNullOrEmpty(imagefilePath) OrElse Not File.Exists(imagefilePath)
+        If condition Then
+            MessageBox.Show("The PowerPoint or iamge file is invalid,Please select an existing file again", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            Return
+        End If
+
+        Try
+            Using presentationDocument__1 As PresentationDocument = PresentationDocument.Open(pptFilePath, True)
+                ' Get the presentation Part of the presentation document
+                Dim presentationPart As PresentationPart = presentationDocument__1.PresentationPart
+                Dim slide As Slide = New InsertImage().InsertSlide(presentationPart, "Title Only")
+                Dim insertImage As New InsertImage()
+                insertImage.InsertImageInLastSlide(slide, imagefilePath, imageExt)
+                slide.Save()
+                presentationDocument__1.PresentationPart.Presentation.Save()
+                MessageBox.Show("Insert Image Successfully,you can check with opening PowerPoint")
+            End Using
+        Catch ex As Exception
+            MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.[Error])
+        End Try
+
+    End Sub
+End Class
