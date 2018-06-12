a0cbe24d3cd90cfa6c951d967be67ca8919aab47
TerryFei <v-tiafe@microsoft.com>
2018-06-12 12:00:59 +0800

    Push project Get desired result by AJAX calls

---
+++ OneCodeTeam/Get-desired-result-by-AJAX-calls-17863/[VB]-Get desired result by AJAX calls/VB/VBASPNETGetDesiredResultByAJAXCalls/Default.aspx.vb
@@ -0,0 +1,24 @@
+﻿'****************************** Module Header ******************************\
+' Module Name:  example.aspx.vb
+' Project:      VBASPNETGetDesiredResultByAJAXCalls
+' Copyright (c) Microsoft Corporation
+'
+' The project illustrates how to get desired result when make AJAX calls to an ASPX page
+' 
+' This source is subject to the Microsoft Public License.
+' See http://www.microsoft.com/en-us/openness/licenses.aspx#MPL.
+' All other rights reserved.
+' 
+' THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, 
+' EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED 
+' WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+'***************************************************************************/
+
+Partial Public Class _Default
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+
+    End Sub
+
+End Class
