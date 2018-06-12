b11c001ad05f194a960f7b9f9c25e8d66b57b75a
TerryFei <v-tiafe@microsoft.com>
2018-06-12 12:21:52 +0800

    Push project Reflection in the .NET Framework for Windows Store Apps

---
+++ OneCodeTeam/Reflection-in-the-NET-Framework-for-Windows-Store-20162/[C#]-Reflection in the .NET Framework for Windows Store Apps/C#/CSWindowsStoreAppReflection/MainPage.xaml.cs
@@ -0,0 +1,142 @@
+﻿/****************************** Module Header ******************************\
+* Module Name:  MainPage.cs
+* Project:	    CSWindowsStoreAppReflection
+* Copyright (c) Microsoft Corporation.
+* 
+* The main UI of this app.
+* 
+* 
+* This source is subject to the Microsoft Public License.
+* See http://www.microsoft.com/en-us/openness/licenses.aspx#MPL.
+* All other rights reserved.
+* 
+* THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, 
+* EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED 
+* WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+\***************************************************************************/
+
+using System;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+namespace CSWindowsStoreAppReflection
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Common.LayoutAwarePage
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// Invoked when this page is about to be displayed in a Frame.
+        /// </summary>
+        /// <param name="e">Event data that describes how this page was reached.  The Parameter
+        /// property is typically used to configure the page.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+
+            // Get current assembly.
+            Assembly asm = typeof(MainPage).GetTypeInfo().Assembly;
+
+            // Bind all classes under CSWindowsStoreAppReflection.Types to typeCombobox
+            typeCombobox.ItemsSource = asm.DefinedTypes
+                .Where(t=>t.IsClass && t.Namespace=="CSWindowsStoreAppReflection.Types");
+
+            // Bind all interfaces under CSWindowsStoreAppReflection.Types to interfaceCombobox
+            interfaceCombobox.ItemsSource = asm.DefinedTypes
+                .Where(t => t.IsInterface && t.Namespace == "CSWindowsStoreAppReflection.Types");
+
+        }
+
+        private void typeCombobox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (sender is ComboBox && (sender as ComboBox).SelectedItem is TypeInfo)
+            {
+
+                // Get the selected type.
+                TypeInfo selectedType = (sender as ComboBox).SelectedItem as TypeInfo;
+
+                // Find all the derived classes of the selected type.
+                Assembly asm = typeof(MainPage).GetTypeInfo().Assembly;
+                var subTypes = asm.DefinedTypes
+                    .Where(t => t.IsClass && t.IsSubclassOf(selectedType.AsType()));
+
+                StringBuilder result = new StringBuilder();
+
+                if (subTypes == null || subTypes.Count() == 0)
+                {
+                    result.Append("There is no class that inherits " + selectedType.FullName);
+                }
+                else
+                {
+                    result.AppendLine("The following classes inherit from" + selectedType.FullName);
+                    result.AppendLine();
+
+                    foreach (var subType in subTypes)
+                    {
+                        result.AppendLine(subType.FullName);
+                    }
+                }
+
+                typeResultText.Text = result.ToString();
+            }
+        }
+
+        private void interfaceCombobox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (sender is ComboBox && (sender as ComboBox).SelectedItem is TypeInfo)
+            {
+
+                // Get the selected interface.
+                TypeInfo selectedInterface = (sender as ComboBox).SelectedItem as TypeInfo;
+
+                // Get all classes that implement the selected interface 
+                Assembly asm = typeof(MainPage).GetTypeInfo().Assembly;
+                var subTypes = asm.DefinedTypes
+                    .Where(t => t.IsClass && t.ImplementedInterfaces.Any(i=> i ==selectedInterface.AsType()));
+
+                StringBuilder result = new StringBuilder();
+
+                if (subTypes == null || subTypes.Count() == 0)
+                {
+                    result.Append("There is no class that implements " + selectedInterface.FullName);
+                }
+                else
+                {
+                    result.AppendLine("The following classes implement " + selectedInterface.FullName);
+                    result.AppendLine();
+
+                    foreach (var subType in subTypes)
+                    {
+                        result.AppendLine(subType.FullName);
+                    }
+                }
+
+                interfaceResultText.Text = result.ToString();
+            }
+        }
+
+
+        #region Common methods
+
+        async private void Footer_Click(object sender, RoutedEventArgs e)
+        {
+            await Windows.System.Launcher.LaunchUriAsync(new Uri((sender as HyperlinkButton).Tag.ToString()));
+        }
+
+        public void NotifyUser(string message)
+        {
+            textStatus.Text = message;
+        }
+
+        #endregion
+    }
+}
---
+++ OneCodeTeam/Reflection-in-the-NET-Framework-for-Windows-Store-20162/[C#]-Reflection in the .NET Framework for Windows Store Apps/C#/CSWindowsStoreAppReflection/Types/Person.cs
@@ -0,0 +1,67 @@
+﻿/****************************** Module Header ******************************\
+* Module Name:  Person.cs
+* Project:	    CSWindowsStoreAppReflection
+* Copyright (c) Microsoft Corporation.
+* 
+* Define the classes to be demonstrated.
+* 
+* Person is the base class.
+* Student inherits from Person, and implements IStudy.
+* Employee inherits from Person, and implements IWork.
+* Engineer and Saler inherit from Employee.
+* 
+* 
+* This source is subject to the Microsoft Public License.
+* See http://www.microsoft.com/en-us/openness/licenses.aspx#MPL.
+* All other rights reserved.
+* 
+* THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND, 
+* EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED 
+* WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+\***************************************************************************/
+
+
+namespace CSWindowsStoreAppReflection.Types
+{
+    public class Person
+    {
+        public double Name { get; set; }
+
+        public int Age { get; set; }
+
+        public bool IsMale { get; set; }
+    }
+
+    public class Student:Person,IStudy
+    {
+        public int Grade { get; set; }
+
+        public string School { get; set; }
+
+
+
+        public void Study()
+        {
+            
+        }
+    }
+
+    public class Employee:Person,IWork
+    {
+        public string Company { get; set; }
+
+        public virtual void Work()
+        {          
+        }
+    }
+
+    public class Engineer:Employee
+    {
+        public string Technology { get; set; }
+    }
+
+    public class Saler:Employee
+    {
+        public string Product { get; set; }
+    }
+}
