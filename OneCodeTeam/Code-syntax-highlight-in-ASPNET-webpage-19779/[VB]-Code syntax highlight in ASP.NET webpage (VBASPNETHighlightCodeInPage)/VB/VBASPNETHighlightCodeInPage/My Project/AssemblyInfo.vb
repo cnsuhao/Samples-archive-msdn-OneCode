ab8519cbe094579486ceb16de16f67a21468286a
TerryFei <v-tiafe@microsoft.com>
2018-06-12 12:08:31 +0800

    Push project Code syntax highlight in ASP.NET webpage (VBASPNETHighlightCodeInPage)

---
+++ OneCodeTeam/Code-syntax-highlight-in-ASPNET-webpage-19779/[VB]-Code syntax highlight in ASP.NET webpage (VBASPNETHighlightCodeInPage)/VB/VBASPNETHighlightCodeInPage/My Project/AssemblyInfo.vb
@@ -0,0 +1,34 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+<Assembly: AssemblyTitle("VBASPNETHighlightCodeInPage")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("Microsoft")> 
+<Assembly: AssemblyProduct("VBASPNETHighlightCodeInPage")> 
+<Assembly: AssemblyCopyright("Copyright © Microsoft 2012")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("bef27bc7-515d-4d32-ab5a-c45d62313781")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
